5004c4a1360776962dde4a34cd5ed455f76b297f
Jamie Li <lijamie@gmail.com>
2022-02-01 11:51:37 -0800

    Convert to Gradle build scripts.

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    `java-library`
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+}
+
+val apacheCommonsLang = "3.12.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+dependencies {
+    // ***************************
+    // Production dependencies
+    // ***************************
+    // From mavenCentral
+    implementation(libs.apache.commons.lang3)
+    implementation(libs.log4j.core)
+    implementation(libs.httpclient)
+    implementation(libs.google.gson)
+    implementation(libs.toml4j)
+    implementation(libs.okhttp3)
+    implementation(libs.commons.codec)
+    implementation(libs.jjwt)
+    implementation(libs.reactor.core)
+    implementation(libs.slf4j.log4j12)
+    implementation(libs.javax.jaxb.api)
+
+    api(libs.lombok)
+    api(libs.servlet.api)
+
+    annotationProcessor(libs.lombok)
+
+    // To be moved
+    implementation(libs.okhttp3.mockserver)
+    implementation(libs.reactor.netty)
+
+    // From jitpack.io
+    implementation(libs.java.stellar.sdk)
+
+    // ***************************
+    // Test dependencies
+    // ***************************
+    testImplementation(libs.kotlin.stdlib)
+    testImplementation(libs.kotlin.junit5)
+    testImplementation(libs.kotlin.mockk)
+    testImplementation(libs.junit5.api)
+    testImplementation(libs.junit5.engine)
+    testImplementation(libs.junit5.params)
+
+    testAnnotationProcessor(libs.lombok)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+rootProject.name = "java-stellar-anchor-sdk"
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            alias("apache.commons.lang3").to("org.apache.commons:commons-lang3:3.12.0")
+            alias("log4j.core").to("org.apache.logging.log4j:log4j-core:2.17.1")
+            alias("httpclient").to("org.apache.httpcomponents:httpclient:4.5.13")
+            alias("google.gson").to("com.google.code.gson:gson:2.8.9")
+            alias("toml4j").to("com.moandjiezana.toml:toml4j:0.7.2")
+            alias("okhttp3").to("com.squareup.okhttp3:okhttp:4.9.3")
+            alias("commons.codec").to("commons-codec:commons-codec:1.15")
+            alias("jjwt").to("io.jsonwebtoken:jjwt:0.9.1")
+            alias("reactor.core").to("io.projectreactor:reactor-core:3.4.14")
+            alias("slf4j.log4j12").to("org.slf4j:slf4j-log4j12:1.7.33")
+            alias("lombok").to("org.projectlombok:lombok:1.18.22")
+            alias("servlet.api").to("javax.servlet:servlet-api:2.5")
+            alias("okhttp3.mockserver").to("com.squareup.okhttp3:mockwebserver:4.9.3")
+            alias("reactor.netty").to("io.projectreactor.netty:reactor-netty:1.0.15")
+            alias("java.stellar.sdk").to("com.github.stellar:java-stellar-sdk:0.29.0")
+
+            alias("kotlin.mockk").to("io.mockk:mockk:1.12.2")
+            alias("kotlin.stdlib").to("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+            alias("kotlin.junit5").to("org.jetbrains.kotlin:kotlin-test-junit5:1.6.10")
+
+            alias("javax.jaxb.api").to("javax.xml.bind:jaxb-api:2.3.1")
+            alias("junit5.api").to("org.junit.jupiter:junit-jupiter-api:5.8.2")
+            alias("junit5.engine").to("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+            alias("junit5.params").to("org.junit.jupiter:junit-jupiter-params:5.8.2")
+
+        }
+    }
+}
