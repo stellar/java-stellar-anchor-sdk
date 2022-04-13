2176426c9dda4c22bdc5339716d3a09165d41e05
Marcelo Salloum <marcelo@stellar.org>
2022-04-13 15:45:24 -0700

    Start handling spring error `HttpMessageNotReadableException` as a 400. It's automatically triggered when the json body cannot be read.

--- platform/src/main/java/org/stellar/anchor/platform/controller/GlobalControllerExceptionHandler.java
+++ platform/src/main/java/org/stellar/anchor/platform/controller/GlobalControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import static org.stellar.anchor.util.Log.errorEx;
 
 import javax.transaction.NotSupportedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,8 +25,16 @@ public class GlobalControllerExceptionHandler {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(MissingServletRequestParameterException.class)
   public SepExceptionResponse handleMissingParams(MissingServletRequestParameterException ex) {
+    errorEx(ex);
     String name = ex.getParameterName();
     return new SepExceptionResponse(String.format("The \"%s\" parameter is missing.", name));
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public SepExceptionResponse handleRandomException(HttpMessageNotReadableException ex) {
+    errorEx(ex);
+    return new SepExceptionResponse("Your request body is wrong in some way.");
   }
 
   @ResponseStatus(HttpStatus.FORBIDDEN)
