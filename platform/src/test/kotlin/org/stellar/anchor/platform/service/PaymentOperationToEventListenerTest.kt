b868a32de9f65f38718dd5e76f591c7f9c3c670e
Marcelo Salloum <marcelo@stellar.org>
2022-04-18 17:20:13 -0700

    Payments whose memo is not in the DB shouldn't trigger event.

--- platform/src/test/kotlin/org/stellar/anchor/platform/service/PaymentOperationToEventListenerTest.kt
+++ platform/src/test/kotlin/org/stellar/anchor/platform/service/PaymentOperationToEventListenerTest.kt
@@ -1,15 +1,14 @@
 package org.stellar.anchor.platform.service
 
-import io.mockk.Called
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.stellar.anchor.event.EventService
+import org.stellar.anchor.model.Sep24Transaction
 import org.stellar.anchor.platform.paymentobserver.ObservedPayment
 import org.stellar.anchor.server.data.JdbcSep31TransactionStore
+import kotlin.test.assertEquals
 
 class PaymentOperationToEventListenerTest {
   @MockK(relaxed = true) private lateinit var transactionStore: JdbcSep31TransactionStore
@@ -27,16 +26,27 @@ class PaymentOperationToEventListenerTest {
   fun test_onReceiver() {
     // Payment missing transaction hash shouldn't trigger an event nor reach the DB
     val p = ObservedPayment.builder().build()
+    p.transactionHash = null
     p.transactionMemo = "my_memo"
     paymentOperationToEventListener.onReceived(p)
     verify { eventService wasNot Called }
     verify { transactionStore wasNot Called }
 
     // Payment missing transaction hash shouldn't trigger an event nor reach the DB
-    p.transactionMemo = null
     p.transactionHash = "1ad62e48724426be96cf2cdb65d5dacb8fac2e403e50bedb717bfc8eaf05af30"
+    p.transactionMemo = null
     paymentOperationToEventListener.onReceived(p)
     verify { eventService wasNot Called }
     verify { transactionStore wasNot Called }
+
+    // Payment whose memo is not in the DB shouldn't trigger event
+    p.transactionHash = "1ad62e48724426be96cf2cdb65d5dacb8fac2e403e50bedb717bfc8eaf05af30"
+    p.transactionMemo = "my_memo"
+    val slotMemo = slot<String>()
+    every { transactionStore.findByStellarMemo(capture(slotMemo)) } returns null
+    paymentOperationToEventListener.onReceived(p)
+    verify { eventService wasNot Called }
+    verify(exactly = 1) { transactionStore.findByStellarMemo(any()) }
+    assertEquals("my_memo", slotMemo.captured)
   }
 }
