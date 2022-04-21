a8b7c156123ef9e442e4db1891729c5e4c1f3b88
Jamie Li <lijamie@gmail.com>
2022-04-21 14:52:26 -0700

    platform: Generalized event listeners and created AnchorEventProcessor (#202)
    * Generalized event listeners and created AnchorEventProcessor
    
    * Merge the reference integration tests with platform integration tests.
    
    * Added FeeServiceTest to have at least one test to avoid CI error

--- anchor-reference-server/src/main/java/org/stellar/anchor/reference/AnchorReferenceConfig.java
+++ anchor-reference-server/src/main/java/org/stellar/anchor/reference/AnchorReferenceConfig.java
@@ -3,6 +3,11 @@ package org.stellar.anchor.reference;
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.stellar.anchor.reference.config.EventSettings;
+import org.stellar.anchor.reference.config.KafkaListenerSettings;
+import org.stellar.anchor.reference.event.AbstractEventListener;
+import org.stellar.anchor.reference.event.AnchorEventProcessor;
+import org.stellar.anchor.reference.event.KafkaListener;
 import org.stellar.anchor.util.GsonUtils;
 
 @Configuration
@@ -10,5 +15,21 @@ public class AnchorReferenceConfig {
   @Bean
   public Gson gson() {
     return GsonUtils.builder().create();
+  }
+
+  @Bean
+  public AbstractEventListener eventListener(
+      EventSettings eventSettings,
+      AnchorEventProcessor anchorEventProcessor,
+      KafkaListenerSettings kafkaListenerSettings) {
+    switch (eventSettings.getListenerType()) {
+      case "kafka":
+        return new KafkaListener(kafkaListenerSettings, anchorEventProcessor);
+      case "sqs":
+      case "amqp":
+      default:
+        throw new RuntimeException(
+            String.format("Invalid event listener: %s", eventSettings.getListenerType()));
+    }
   }
 }
