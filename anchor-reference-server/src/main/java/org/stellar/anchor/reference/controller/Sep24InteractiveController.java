524f172c3ef3d0f5a78ff8c5ceedaafa82dddbf2
Jamie Li <lijamie@gmail.com>
2022-04-27 16:47:03 -0700

    Added SEP-24 skeleton (#218)
    * Added SEP-24 skeleton
    
    * Improve according to PR reviews.
    
    * Added sep24 configuration to yaml

---
+++ anchor-reference-server/src/main/java/org/stellar/anchor/reference/controller/Sep24InteractiveController.java
@@ -0,0 +1,29 @@
+package org.stellar.anchor.reference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.stellar.anchor.exception.SepValidationException;
+
+import javax.servlet.http.HttpServletRequest;
+
+/** The controller that implement the endpoints of the Sep24 interactive flow. */
+@Controller
+@CrossOrigin(origins = "*")
+@RequestMapping("/")
+public class Sep24InteractiveController {
+  @CrossOrigin(origins = "*")
+  @RequestMapping(
+      value = "/sep24/interactive",
+      method = {RequestMethod.GET})
+  @ResponseBody
+  public String interactive(HttpServletRequest request) throws SepValidationException {
+    String operation = request.getParameter("operation");
+    if (operation == null) throw new SepValidationException("Missing [operation] parameter.");
+    if (operation.equals("withdraw")) return "The sep24 interactive WITHDRAW starts here.";
+    else if (operation.equals("deposit")) return "The sep24 interactive DEPOSIT starts here.";
+    else return "Undefined operation " + operation;
+  }
+}
