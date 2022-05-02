1dba4b9d2beba9b895f02bff63675b49b8e8b990
Marcelo Salloum <marcelo@stellar.org>
2022-05-02 14:17:37 -0700

    [WIP] Implement Circle memo generator in Sep31Service.

--- core/src/test/kotlin/org/stellar/anchor/sep31/Sep31ServiceTest.kt
+++ core/src/test/kotlin/org/stellar/anchor/sep31/Sep31ServiceTest.kt
@@ -3,14 +3,16 @@ package org.stellar.anchor.sep31
 import com.google.gson.Gson
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
+import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 import java.util.*
 import org.apache.commons.lang3.StringUtils
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.stellar.anchor.Constants
 import org.stellar.anchor.api.callback.CustomerIntegration
 import org.stellar.anchor.api.callback.FeeIntegration
@@ -362,9 +364,35 @@ internal class Sep31ServiceTest {
         Sep31Transaction::class.java
       )
     assert(generateTransactionMemoMethod.trySetAccessible())
-    @Suppress("UNCHECKED_CAST") generateTransactionMemoMethod.invoke(sep31Service, txn)
+    assertDoesNotThrow { generateTransactionMemoMethod.invoke(sep31Service, txn) }
 
     assertEquals("hash", txn.stellarMemoType)
     assertEquals("YTIzOTJhZGQtODdjOS00MmYwLWE1YzEtNWYxNzI4MDM=", txn.stellarMemo)
+  }
+
+  @Test
+  fun test_calculateMemoCircle() {
+    every { sep31Config.memoGenerator } returns Sep31Config.MemoGenerator.CIRCLE
+
+    assertEquals("a2392add-87c9-42f0-a5c1-5f1728030b68", txn.id)
+    assertNull(txn.stellarMemoType)
+    assertNull(txn.stellarMemo)
+
+    // TODO: mock Circle requests/responses
+
+    val generateTransactionMemoMethod: Method =
+      Sep31Service::class.java.getDeclaredMethod(
+        "generateTransactionMemo",
+        Sep31Transaction::class.java
+      )
+    assert(generateTransactionMemoMethod.trySetAccessible())
+    val ex: InvocationTargetException = assertThrows {
+      generateTransactionMemoMethod.invoke(sep31Service, txn)
+    }
+    assertEquals("Not implemented", ex.targetException.message)
+
+    // TODO: assert updated memo
+    //    assertEquals("hash", txn.stellarMemoType)
+    //    assertEquals("YTIzOTJhZGQtODdjOS00MmYwLWE1YzEtNWYxNzI4MDM=", txn.stellarMemo)
   }
 }
