ea0e37927ae8960b0923223ed30a495b790881c3
Marcelo Salloum <marcelo@stellar.org>
2022-05-12 16:37:53 -0700

    Implement the `SepClient#httpPost` method.

--- integration-tests/src/main/kotlin/org/stellar/anchor/platform/SepClient.kt
+++ integration-tests/src/main/kotlin/org/stellar/anchor/platform/SepClient.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.springframework.http.HttpStatus
 import org.stellar.anchor.api.exception.SepException
@@ -22,16 +23,27 @@ open class SepClient {
         .build()
   }
   fun httpGet(url: String, jwt: String? = null): String? {
-    var builder = Request.Builder().url("$url").header("Content-Type", "application/json").get()
-
+    var builder = Request.Builder().url(url).header("Content-Type", "application/json").get()
     if (jwt != null) {
       builder = builder.header("Authorization", "Bearer $jwt")
     }
     val request = builder.build()
+
     val response = client.newCall(request).execute()
     return handleResponse(response)
   }
-  fun httpPostWithJwt(request: Any, jwt: String) {}
+  fun httpPost(url: String, requestBody: Map<String, Any>, jwt: String? = null): String? {
+    val requestBodyStr = gson.toJson(requestBody).toRequestBody(TYPE_JSON)
+
+    var builder = Request.Builder().url(url).header("Content-Type", "application/json").post(requestBodyStr)
+    if (jwt != null) {
+      builder = builder.header("Authorization", "Bearer $jwt")
+    }
+    val request = builder.build()
+
+    val response = client.newCall(request).execute()
+    return handleResponse(response)
+  }
 
   fun handleResponse(response: Response): String? {
     val responseBody = response.body?.string()
