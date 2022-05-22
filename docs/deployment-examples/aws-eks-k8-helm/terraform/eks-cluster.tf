c89b9dae4d2323eccef1964a3c5160b262398d54
reecexlm <reecexlm@gmail.com>
2022-05-22 13:53:21 -0700

    mini to 6

--- docs/deployment-examples/aws-eks-k8-helm/terraform/eks-cluster.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/eks-cluster.tf
@@ -330,7 +330,7 @@ module "eks" {
       capacity_type = "SPOT"
       instance_types = ["t3.micro"]
       desired_size = 6
-      min_size     = 5
+      min_size     = 6
       max_size     = 8
 
         tags = {
