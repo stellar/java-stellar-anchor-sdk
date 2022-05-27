6393d024950dd6c26ecd87dbbd747da32f2b8746
reecexlm <reecexlm@gmail.com>
2022-05-27 15:02:19 -0700

    fix sg

--- docs/deployment-examples/aws-fargate-ecs/terraform/security-groups.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/security-groups.tf
@@ -48,24 +48,24 @@ resource "aws_security_group" "ref_alb" {
   }
 }
 
-resource "aws_security_group" "ecs_tasks" {
-  name   = "${var.environment}-anchorplatform-sg-task"
-  vpc_id = module.vpc.vpc_id
+#resource "aws_security_group" "ecs_tasks" {
+#  name   = "${var.environment}-anchorplatform-sg-task"
+#  vpc_id = module.vpc.vpc_id
  
-  ingress {
-   protocol         = "tcp"
-   from_port        = var.container_port
-   to_port          = var.container_port
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
-  }
+#  ingress {
+#   protocol         = "tcp"
+#   from_port        = var.container_port
+#   to_port          = var.container_port
+#   cidr_blocks      = ["0.0.0.0/0"]
+##   ipv6_cidr_blocks = ["::/0"]
+ # }
  
-  egress {
-   protocol         = "-1"
-   from_port        = 0
-   to_port          = 0
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#  egress {
+#   protocol         = "-1"
+#   from_port        = 0
+#   to_port          = 0
+#   cidr_blocks      = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks = ["::/0"]
+#  }
+#}
 
