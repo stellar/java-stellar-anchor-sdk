7934dbb344ab5326a0590699157b1891b0601180
reecexlm <reecexlm@gmail.com>
2022-05-27 14:39:12 -0700

    remove hosted zone var

--- docs/deployment-examples/aws-fargate-ecs/terraform/variables.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/variables.tf
@@ -1,26 +1,3 @@
-variable "map_users" {
-  description = "additional iam users to add to the aws-auth configmap"
-  type = list(object({
-    userarn = string
-    username = string
-    groups = list(string)
-  }))
-  
-  default = [
-    {
-      userarn  = "arn:aws:iam::245943599471:user/reece"
-      username = "reece"
-      groups   = ["system:masters"]
-    },
- 
-  ]
-}
-variable "hosted_zone_name" {
-  description = "name of hosted zone for anchor platform"
-  type = string
-  #default = "stellaranchordemo.com"
-}
-
 variable "environment" {
   description = "deployment environment"
   type = string
