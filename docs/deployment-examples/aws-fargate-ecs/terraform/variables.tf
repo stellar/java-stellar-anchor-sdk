82d438c1839beb3c56ab4cba622bf1d8ab6e6873
reecexlm <reecexlm@gmail.com>
2022-05-28 10:19:59 -0700

    var

--- docs/deployment-examples/aws-fargate-ecs/terraform/variables.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/variables.tf
@@ -3,3 +3,9 @@ variable "environment" {
   type = string
   default = "dev"
 }
+
+variable "hosted_zone_name" {
+  description = "name of hosted zone for anchor platform"
+  type = string
+  #default = "stellaranchordemo.com"
+}
