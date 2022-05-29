8c9174b6bf5fac5713581660942f68691d57a492
reecexlm <reecexlm@gmail.com>
2022-05-29 09:15:56 -0700

    acme

--- docs/deployment-examples/aws-eks-k8-helm/terraform/versions.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    acme = {
-      source  = "vancluever/acme"
-      version = "~> 2.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.20.0"
