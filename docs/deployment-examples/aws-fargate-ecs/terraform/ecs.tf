bb097102cbe534d97d3cbff03ac2278931a34fb4
reecexlm <reecexlm@gmail.com>
2022-05-31 23:22:00 -0700

    30sec

--- docs/deployment-examples/aws-fargate-ecs/terraform/ecs.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/ecs.tf
@@ -205,7 +205,7 @@ resource "aws_alb_target_group" "sep" {
  
   health_check {
    healthy_threshold   = "2"
-   interval            = "300"
+   interval            = "30"
    protocol            = "HTTP"
    matcher             = "200"
    timeout             = "3"
