ff3c1310c69d927b01aa6bfdb10b75c778d25b10
reecexlm <reecexlm@gmail.com>
2022-06-01 05:51:50 -0700

    reevert #

--- docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
@@ -25,28 +25,28 @@ resource "aws_route53_record" "ref" {
   records = [aws_lb.ref.dns_name]
 }
 
-#resource "tls_private_key" "private_key" {
-#  algorithm = "RSA"
-#}
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
 
-#resource "acme_registration" "registration" {
-#  account_key_pem = tls_private_key.private_key.private_key_pem
-#  email_address   = "reece@stellar.org" 
-#  depends_on = [aws_route53_record.sep, tls_private_key.private_key]
-#}
+resource "acme_registration" "registration" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = "reece@stellar.org" 
+  depends_on = [aws_route53_record.sep, tls_private_key.private_key]
+}
 
-#resource "acme_certificate" "certificate" {
-#  account_key_pem           = acme_registration.registration.account_key_pem
-#  common_name               = "www.${data.aws_route53_zone.anchor-zone.name}"
-  #subject_alternative_names = ["*.${data.aws_route53_zone.anchor-zone.name}"]
+resource "acme_certificate" "certificate" {
+  account_key_pem           = acme_registration.registration.account_key_pem
+  common_name               = "www.${data.aws_route53_zone.anchor-zone.name}"
+  subject_alternative_names = ["*.${data.aws_route53_zone.anchor-zone.name}"]
 
-#  dns_challenge {
-#    provider = "route53"
+  dns_challenge {
+    provider = "route53"
 
- #   config = {
- ##     AWS_HOSTED_ZONE_ID = data.aws_route53_zone.anchor-zone.zone_id
-  #  }
- # }
+    config = {
+      AWS_HOSTED_ZONE_ID = data.aws_route53_zone.anchor-zone.zone_id
+    }
+  }
 
   #depends_on = [aws_route53_record.sep, acme_registration.registration]
 #}
@@ -71,9 +71,9 @@ resource "aws_acm_certificate_validation" "acm_certificate_validation" {
 
 
 
-#resource "aws_iam_server_certificate" "sep_cert" {
-#  name             = "sepcert"
-#  certificate_body = acme_certificate.certificate.certificate_pem
-#  private_key      = acme_certificate.certificate.private_key_pem
-#}
+resource "aws_iam_server_certificate" "sep_cert" {
+  name             = "sepcert"
+  certificate_body = acme_certificate.certificate.certificate_pem
+  private_key      = acme_certificate.certificate.private_key_pem
+}
 
