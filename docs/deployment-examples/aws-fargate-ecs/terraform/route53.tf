9001c8f57329aa5980d7a575db5f26114037b6d2
reecexlm <reecexlm@gmail.com>
2022-06-01 06:01:49 -0700

    sepsep

--- docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
@@ -9,13 +9,36 @@ data "aws_route53_zone" "anchor-zone" {
   private_zone = false
 }
 
-resource "aws_route53_record" "sep" {
-  zone_id = data.aws_route53_zone.anchor-zone.zone_id
-  name    = "www.${data.aws_route53_zone.anchor-zone.name}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.sep.dns_name]
+data "aws_route53_zone" "sep" {
+  name         = "www.stellaranchordemo.com"
+  private_zone = false
 }
+
+#resource "aws_route53_record" "sep" {
+#  zone_id = data.aws_route53_zone.anchor-zone.zone_id
+#  name    = "www.${data.aws_route53_zone.anchor-zone.name}"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [aws_lb.sep.dns_name]
+#}
+
+resource "aws_route53_record" "sep" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}
+
 
 resource "aws_route53_record" "ref" {
   zone_id = data.aws_route53_zone.anchor-zone.zone_id
@@ -61,7 +84,7 @@ lifecycle {
 
 resource "aws_acm_certificate_validation" "acm_certificate_validation" {
  certificate_arn = aws_acm_certificate.acm_certificate.arn
- validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+ validation_record_fqdns = [for record in aws_route53_record.sep : record.fqdn]
 }
 
 
