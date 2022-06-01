54a5e5b5052b0ec34ca09266b21856e0d9773ce3
reecexlm <reecexlm@gmail.com>
2022-06-01 05:50:02 -0700

    validation

--- docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
@@ -61,7 +61,11 @@ lifecycle {
 
 resource "aws_acm_certificate_validation" "acm_certificate_validation" {
  certificate_arn = aws_acm_certificate.acm_certificate.arn
- validation_record_fqdns = [ aws_route53_record.sep.fqdn]
+ name    = aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_type
+  zone_id = aws_route53_zone.anchor-zone.zone_id
+  records = aws_acm_certificate.acm_certificate.domain_validation_options.0.resource_record_value
+  ttl     = "60"
 }
 
 
