1ea0fecb0f628b75335d95a65483f035dcb63950
reecexlm <reecexlm@gmail.com>
2022-06-01 06:44:41 -0700

    x acme for reals

--- docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/route53.tf
@@ -1,20 +1,12 @@
-provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+#provider "acme" {
+#  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 #  server_url = "https://acme-v02.api.letsencrypt.org/directory"
   #server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
+#}
 
 data "aws_route53_zone" "anchor-zone" {
   name         = "${var.hosted_zone_name}"
   private_zone = false
-}
-
-resource "aws_route53_record" "sepwww" {
-  zone_id = data.aws_route53_zone.anchor-zone.zone_id
-  name    = "www.${data.aws_route53_zone.anchor-zone.name}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.sep.dns_name]
 }
 
 resource "aws_route53_record" "sep" {
@@ -43,15 +35,15 @@ resource "aws_route53_record" "ref" {
   records = [aws_lb.ref.dns_name]
 }
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
+#resource "tls_private_key" "private_key" {
+#  algorithm = "RSA"
+#}
 
-resource "acme_registration" "registration" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = "reece@stellar.org" 
-  depends_on = [aws_route53_record.sepwww, tls_private_key.private_key]
-}
+#resource "acme_registration" "registration" {
+#  account_key_pem = tls_private_key.private_key.private_key_pem
+#  email_address   = "reece@stellar.org" 
+#  depends_on = [aws_route53_record.sepwww, tls_private_key.private_key]
+#}
 
 #resource "acme_certificate" "certificate" {
 #  account_key_pem           = acme_registration.registration.account_key_pem
