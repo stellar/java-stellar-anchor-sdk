be67ad7821447563ee387759fe09e2002260bdc6
reecexlm <reecexlm@gmail.com>
2022-05-31 20:54:55 -0700

    ecs_task_execution_role

--- docs/deployment-examples/aws-fargate-ecs/terraform/ecs.tf
+++ docs/deployment-examples/aws-fargate-ecs/terraform/ecs.tf
@@ -304,6 +304,6 @@ EOF
 
  
 resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
-  role       = aws_iam_role.ecs_task_role.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.create_log_group.arn
 }
