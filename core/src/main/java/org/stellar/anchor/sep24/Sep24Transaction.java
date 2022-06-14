975575bc9d7dfa395c1dff425794f856459fdb77
Jamie Li <lijamie@gmail.com>
2022-06-14 12:15:02 +0800

    Fixed JavaDoc errors

--- core/src/main/java/org/stellar/anchor/sep24/Sep24Transaction.java
+++ core/src/main/java/org/stellar/anchor/sep24/Sep24Transaction.java
@@ -218,7 +218,7 @@ public interface Sep24Transaction {
   /**
    * Amount sent by anchor to user at end of transaction as a string with up to 7 decimals. Excludes amount converted to XLM to fund account and any external fees.
    *
-   * @return <code>amount_out<code> field of the SEP-24 transaction history.
+   * @return <code>amount_out</code> field of the SEP-24 transaction history.
    */
   String getAmountOut();
 
