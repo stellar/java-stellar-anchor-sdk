7456b40810ff1a871fab869eec041257d0e6afaa
Marcelo Salloum <marcelo@stellar.org>
2022-06-27 15:29:18 -0700

    Code formatted on pre-commit.

--- platform/src/main/java/org/stellar/anchor/platform/condition/ConditionalOnAllSepsEnabled.java
+++ platform/src/main/java/org/stellar/anchor/platform/condition/ConditionalOnAllSepsEnabled.java
@@ -1,8 +1,7 @@
 package org.stellar.anchor.platform.condition;
 
-import org.springframework.context.annotation.Conditional;
-
 import java.lang.annotation.*;
+import org.springframework.context.annotation.Conditional;
 
 /** Indicates if any of the SEPs in the "seps" list is enabled in the configuration. */
 @Target({ElementType.TYPE, ElementType.METHOD})
