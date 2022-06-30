27c9500c9e448909b4ba2417cdf74a015719433e
reecexlm <reecexlm@gmail.com>
2022-06-30 07:56:27 -0700

    debug

--- docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
+++ docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-echo "hello world1"
+echo "ls /config"
 ls /config
-cp /config_files/* /anchor_config
-echo "hello world2"
+cp -v /config_files/* /anchor_config
+echo "ls /anchor_config"
 ls /anchor_config
 echo "hello world3"
 echo "tailing for eternity..."
 tail -f /dev/null
+    
