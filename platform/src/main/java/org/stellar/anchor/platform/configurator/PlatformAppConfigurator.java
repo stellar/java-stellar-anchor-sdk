c0145eb3087a431f8bc2a05fb6d71c3492628275
reecexlm <reecexlm@gmail.com>
2022-06-30 11:57:59 -0700

    add some debugging

--- platform/src/main/java/org/stellar/anchor/platform/configurator/PlatformAppConfigurator.java
+++ platform/src/main/java/org/stellar/anchor/platform/configurator/PlatformAppConfigurator.java
@@ -4,16 +4,19 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.stellar.anchor.util.Log;
 
 public class PlatformAppConfigurator extends AbstractConfigurator
     implements ApplicationContextInitializer<ConfigurableApplicationContext> {
   @Override
   public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
     // Find the location of the data-access settings.
-    String path = getFlatProperties().getProperty("stellar.anchor.app-config.settings");
 
+    String path = getFlatProperties().getProperty("stellar.anchor.app-config.settings");
+    Log.debug("REECEDEBUG prop path = '%s", path);
     // Load and add the data access settings to Spring `Environment`
     PropertiesPropertySource pps = createPrefixedPropertySource(path);
+    Log.debug("REECEDEBUG pps = '%s", pps.toString());
     applicationContext.getEnvironment().getPropertySources().addFirst(pps);
   }
 }
