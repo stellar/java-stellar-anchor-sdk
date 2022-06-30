0cbc51138866da0269bb134d7537b90161e4d5a0
reecexlm <reecexlm@gmail.com>
2022-06-30 16:58:52 -0700

    fix copy config

--- docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
+++ docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 echo "ls /config"
 ls /config
-cp -v /config_files/* /anchor_config
+cp -v /config/stellar_wks.toml /anchor_config
+cp -v /config/anchor_config /anchor_config
 echo "ls /anchor_config"
 ls /anchor_config
 echo "hello world3"
