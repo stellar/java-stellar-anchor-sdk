fccf9c63e7e73d7b603a6c16f9dd120874a0fc05
reecexlm <reecexlm@gmail.com>
2022-06-30 11:10:12 -0700

    TRACE

--- docs/resources/deployment-examples/aws-fargate-ecs/sep.sh
+++ docs/resources/deployment-examples/aws-fargate-ecs/sep.sh
@@ -6,4 +6,6 @@ ls /app
 echo "env"
 env
 echo "starting anchor platform..."
+cat /anchor_config/anchor_config.yaml
+export _JAVA_OPTIONS=-Dlogging.level.org.springframework=TRACE
 java -jar /app/anchor-platform-runner.jar --sep-server
