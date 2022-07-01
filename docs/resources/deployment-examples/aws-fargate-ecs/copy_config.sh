493d0352ca8cf8b64f91d45e87ca4af0f726ab7b
reecexlm <reecexlm@gmail.com>
2022-06-30 18:34:51 -0700

    sep.sh

--- docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
+++ docs/resources/deployment-examples/aws-fargate-ecs/copy_config.sh
@@ -3,6 +3,7 @@ echo "ls /config"
 ls /config
 cp -v /config_files/stellar_wks.toml /anchor_config
 cp -v /config_files/anchor_config.yaml /anchor_config
+cp -v /config_files/sep.sh /anchor_config
 cat  /anchor_config/anchor_config.yaml
 cat  /anchor_config/stellar_wks.toml
 echo "ls /anchor_config"
