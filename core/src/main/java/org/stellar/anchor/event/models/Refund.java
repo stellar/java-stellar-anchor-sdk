e17cc360634e48a95b24b17a57013a8ca328c008
Stephen <stfung77@gmail.com>
2022-07-06 22:58:10 -0400

    use snake case for kafka events (#374)

--- core/src/main/java/org/stellar/anchor/event/models/Refund.java
+++ core/src/main/java/org/stellar/anchor/event/models/Refund.java
@@ -1,5 +1,6 @@
 package org.stellar.anchor.event.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import java.time.Instant;
 import lombok.AllArgsConstructor;
@@ -14,9 +15,11 @@ public class Refund {
   String type;
   Amount amount;
 
+  @JsonProperty("requested_at")
   @SerializedName("requested_at")
   Instant requestedAt;
 
+  @JsonProperty("refunded_at")
   @SerializedName("refunded_at")
   Instant refundedAt;
 
