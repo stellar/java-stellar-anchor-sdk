3b6e03074e52e547c098afc13e8c4eba995cead2
reecexlm <reecexlm@gmail.com>
2022-07-14 10:40:04 -0700

    sample files fix

--- docs/resources/deployment-examples/example-fargate/terraform/codebuild_config.tf
+++ docs/resources/deployment-examples/example-fargate/terraform/codebuild_config.tf
@@ -36,14 +36,14 @@ resource "aws_s3_bucket_object" "anchor_config" {
 }
 resource "aws_s3_bucket_object" "reference_config" {
   bucket = aws_s3_bucket.anchor_config.id
-  key    = "anchor_config.yaml"
+  key    = "reference_config.yaml"
   acl    = "private"  # or can be "public-read"
   source = "../example_config/reference_config.yaml"
   etag = filemd5("../example_config/reference_config.yaml")
 }
 resource "aws_s3_bucket_object" "stellar_toml" {
   bucket = aws_s3_bucket.anchor_config.id
-  key    = "anchor_config.yaml"
+  key    = "stellar.toml"
   acl    = "private"  # or can be "public-read"
   source = "../example_config/stellar.toml"
   etag = filemd5("../example_config/stellar.toml")
