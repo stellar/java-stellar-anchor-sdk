631544e3468356adfa411c70740a5d7a65573060
Stephen <stfung77@gmail.com>
2022-07-18 12:51:57 -0400

    Remove liquibase and references to it (#391)
    Remove liquibase and references to it

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,6 @@ subprojects {
     implementation("org.springframework.kafka:spring-kafka:2.8.4")
     implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
     implementation("org.postgresql:postgresql:42.3.5")
-    implementation("org.liquibase:liquibase-core:4.10.0")
     implementation("org.apache.logging.log4j:log4j-layout-template-json:2.14.1")
 
     // The common dependencies are declared here because we would like to have a uniform unit
