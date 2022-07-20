a4656d428080ca2463d94e63d21bdb95b9c13c79
Marcelo Salloum <marcelo@stellar.org>
2022-07-20 14:20:55 -0700

    Change default log appender.

--- platform/src/test/kotlin/org/stellar/anchor/platform/LogAppenderTest.kt
+++ platform/src/test/kotlin/org/stellar/anchor/platform/LogAppenderTest.kt
@@ -1,10 +1,9 @@
 package org.stellar.anchor.platform
 
-import com.google.gson.*
-import com.google.gson.reflect.TypeToken
 import java.io.*
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.*
 import org.stellar.anchor.util.*
 
 class LogAppenderTest {
@@ -13,13 +12,11 @@ class LogAppenderTest {
     val outputStreamCaptor = ByteArrayOutputStream()
     System.setOut(PrintStream(outputStreamCaptor))
     Log.info("hello world")
-    val json = outputStreamCaptor.toString().trim()
+    val outputStream = outputStreamCaptor.toString().trim()
 
-    val gson = Gson()
-    val type = object : TypeToken<Map<String?, *>?>() {}.type
-    var actualLog: Map<String, Any>? = null
-
-    assertDoesNotThrow { actualLog = gson.fromJson(json, type) }
-    assertEquals("{message=hello world, severity=INFO}", actualLog!!["event"].toString())
+    MatcherAssert.assertThat(
+      outputStream,
+      CoreMatchers.endsWith("INFO  - o.s.a.p.LogAppenderTest - hello world")
+    )
   }
 }
