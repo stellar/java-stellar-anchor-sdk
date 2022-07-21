27058aff66d64097b9e542a03a7d1e1f65c6b57a
Erika Lee <erika@stellar.org>
2022-07-21 16:39:41 -0700

    Add empty validation checks for circle payment observer, sep 10, 12, and 38.

--- platform/src/main/java/org/stellar/anchor/platform/config/PropertySep38Config.java
+++ platform/src/main/java/org/stellar/anchor/platform/config/PropertySep38Config.java
@@ -1,10 +1,26 @@
 package org.stellar.anchor.platform.config;
 
 import lombok.Data;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 import org.stellar.anchor.config.Sep38Config;
 
 @Data
-public class PropertySep38Config implements Sep38Config {
+public class PropertySep38Config implements Sep38Config, Validator {
   boolean enabled = false;
   String quoteIntegrationEndPoint;
+
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return Sep38Config.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public void validate(Object target, Errors errors) {
+    Sep38Config config = (Sep38Config) target;
+
+    ValidationUtils.rejectIfEmpty(
+        errors, "quoteIntegrationEndPoint", "empty-quoteIntegrationEndPoint");
+  }
 }
