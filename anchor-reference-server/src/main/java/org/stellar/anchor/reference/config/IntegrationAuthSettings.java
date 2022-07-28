b488cd4c498c57657399adad0a91e2ece60b4fef
Jamie Li <lijamie@gmail.com>
2022-07-28 11:08:27 -0700

    add integration-auth back to the @ConfigurationProperties

--- anchor-reference-server/src/main/java/org/stellar/anchor/reference/config/IntegrationAuthSettings.java
+++ anchor-reference-server/src/main/java/org/stellar/anchor/reference/config/IntegrationAuthSettings.java
@@ -1,11 +1,13 @@
 package org.stellar.anchor.reference.config;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.stellar.anchor.config.IntegrationAuthConfig.AuthType;
 
 @Data
 @Configuration
+@ConfigurationProperties(prefix = "integration-auth")
 public class IntegrationAuthSettings {
   AuthType authType = AuthType.NONE;
   String platformToAnchorSecret;
