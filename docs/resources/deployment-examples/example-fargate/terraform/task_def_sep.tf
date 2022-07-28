a8f920a24c58f9f1e49791670f386b31270828e4
reecexlm <98427531+reecexlm@users.noreply.github.com>
2022-07-28 12:42:47 -0700

    Fixed issues with deployment (#439)
    * reference server uses public image
    
    * fix reference server
    
    * fix config image for ref
    
    * update secrets
    
    * authType: NONE
    
    * sep health check
    
    * wait 2 minutes for startup
    
    * remove healthcheck until docker has curl command
    
    * set multiline patern
    
    * escape \
    
    * sepservice log config
    
    * disable metrics service
    
    * test with h2
    
    * spring.flyway.enabled: false
    
    * debug in toml
    
    * remove file

--- docs/resources/deployment-examples/example-fargate/terraform/task_def_sep.tf
+++ docs/resources/deployment-examples/example-fargate/terraform/task_def_sep.tf
@@ -26,10 +26,11 @@ resource "aws_ecs_task_definition" "sep" {
    logConfiguration = {
                 "logDriver": "awslogs",
                 "options": {
-                    "awslogs-group": "anchor-platform",
+                    "awslogs-group": "anchorplatform",
                     "awslogs-region": "${var.aws_region}",
                     "awslogs-create-group": "true",
-                    "awslogs-stream-prefix": "sep"
+                    "awslogs-stream-prefix": "sep-config",
+                    "awslogs-multiline-pattern": "^[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3} \\w+ -", 
                 }
             }
   },{
@@ -70,6 +71,10 @@ resource "aws_ecs_task_definition" "sep" {
       {
         "name": "PLATFORM_TO_ANCHOR_SECRET",
         "valueFrom": data.aws_ssm_parameter.platform_to_anchor_secret.arn
+      },
+      {
+        "name": "ANCHOR_TO_PLATFORM_SECRET",
+        "valueFrom": data.aws_ssm_parameter.anchor_to_platform_secret.arn
       }
    ]
 
@@ -90,14 +95,16 @@ resource "aws_ecs_task_definition" "sep" {
                   "value": "TEST3"
               }
           ],
-      logConfiguration = {
+       logConfiguration = {
                 "logDriver": "awslogs",
                 "options": {
-                    "awslogs-group": "anchor-platform",
+                    "awslogs-group": "anchorplatform",
                     "awslogs-region": "${var.aws_region}",
                     "awslogs-create-group": "true",
-                    "awslogs-stream-prefix": "sep"
+                    "awslogs-stream-prefix": "sep-service",
+                    "awslogs-multiline-pattern": "^[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3} \\w+ -", 
                 }
+            
             }
    portMappings = [{
      protocol      = "tcp"
