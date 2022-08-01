661951b946c0346331c1b4561576369411dc5754
Jamie Li <lijamie@gmail.com>
2022-08-01 13:52:12 -0700

    Fix the test error in IntelliJ but not in gradle

--- platform/src/test/kotlin/org/stellar/anchor/platform/LogAppenderTest.kt
+++ platform/src/test/kotlin/org/stellar/anchor/platform/LogAppenderTest.kt
@@ -1,22 +1,30 @@
 package org.stellar.anchor.platform
 
-import java.io.*
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import io.mockk.*
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.Appender
+import org.apache.logging.log4j.core.LogEvent
+import org.apache.logging.log4j.core.LoggerContext
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.stellar.anchor.util.*
 
 class LogAppenderTest {
   @Test
-  fun test_logJsonAppenderFormat() {
-    val outputStreamCaptor = ByteArrayOutputStream()
-    System.setOut(PrintStream(outputStreamCaptor))
-    Log.info("hello world")
-    val outputStream = outputStreamCaptor.toString().trim()
+  fun test_loggerInfo_accurateLoggerName() {
+    val appender = mockk<Appender>()
+    val capturedLogEvent = slot<LogEvent>()
 
-    MatcherAssert.assertThat(
-      outputStream,
-      CoreMatchers.endsWith("INFO  - o.s.a.p.LogAppenderTest - hello world")
-    )
+    every { appender.name } returns "mock appender"
+    every { appender.isStarted } returns true
+    every { appender.append(capture(capturedLogEvent)) }
+
+    val loggerContext: LoggerContext = LogManager.getContext(false) as LoggerContext
+    val loggerConfig = loggerContext.configuration.getLoggerConfig("root")
+    loggerConfig.addAppender(appender, Level.ALL, null)
+
+    Log.info("hello world")
+    assertEquals(LogAppenderTest::class.qualifiedName, capturedLogEvent.captured.loggerName)
   }
 }
