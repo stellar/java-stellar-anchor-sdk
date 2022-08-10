3ddb1818853b68063e32248813cdfd1d3af38847
Jamie Li <lijamie@gmail.com>
2022-08-10 16:40:20 -0700

    Gradle clean up and define dependency aliases in `libs.versions.toml` file. (#477)
    * Alias non-spring dependencies
    
    * use aliases in build gradle files
    
    * use alias in plugin scope
    
    * upgraded kotlin to 1.6.20
    
    * fixed the kotlin plugin loaded multiple time issue
    
    * add the missing alias org.springframework.cloud:spring-cloud-aws-messaging

--- anchor-reference-server/build.gradle.kts
+++ anchor-reference-server/build.gradle.kts
@@ -1,8 +1,10 @@
+// The alias call in plugins scope produces IntelliJ false error which is suppressed here.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
   application
-  id("org.springframework.boot") version "2.6.3"
-  id("io.spring.dependency-management") version "1.0.11.RELEASE"
-  id("org.jetbrains.kotlin.jvm") version "1.6.10"
+  alias(libs.plugins.spring.boot)
+  alias(libs.plugins.spring.dependency.management)
+  alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
@@ -12,7 +14,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-autoconfigure")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("com.h2database:h2")
+
+  implementation(libs.spring.aws.messaging)
+  implementation(libs.spring.kafka)
+  implementation(libs.h2database)
   implementation(libs.sqlite.jdbc)
   implementation(libs.google.gson)
   implementation(libs.java.stellar.sdk)
@@ -22,10 +27,6 @@ dependencies {
   implementation(project(":core"))
 
   annotationProcessor(libs.lombok)
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 application { mainClass.set("org.stellar.anchor.reference.AnchorReferenceServer") }
-
