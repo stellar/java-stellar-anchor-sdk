78f75216d3d786c2617f6e1f8de7876fd7f1e743
Jamie Li <lijamie@gmail.com>
2022-08-11 10:57:40 -0700

    upsert stellar account to payment observing manager when Sep31DepositInfoGeneratorApi receives a new account from the anchor

--- platform/src/main/java/org/stellar/anchor/platform/service/Sep31DepositInfoGeneratorApi.java
+++ platform/src/main/java/org/stellar/anchor/platform/service/Sep31DepositInfoGeneratorApi.java
@@ -4,16 +4,22 @@ import java.util.Objects;
 import org.stellar.anchor.api.callback.GetUniqueAddressResponse;
 import org.stellar.anchor.api.callback.UniqueAddressIntegration;
 import org.stellar.anchor.api.exception.AnchorException;
+import org.stellar.anchor.api.exception.InternalServerErrorException;
 import org.stellar.anchor.api.sep.sep31.Sep31DepositInfo;
+import org.stellar.anchor.platform.payment.observer.stellar.PaymentObservingAccountsManager;
 import org.stellar.anchor.sep31.Sep31DepositInfoGenerator;
 import org.stellar.anchor.sep31.Sep31Transaction;
 import org.stellar.anchor.util.MemoHelper;
 
 public class Sep31DepositInfoGeneratorApi implements Sep31DepositInfoGenerator {
   private UniqueAddressIntegration uniqueAddressIntegration;
+  private PaymentObservingAccountsManager paymentObservingAccountsManager;
 
-  public Sep31DepositInfoGeneratorApi(UniqueAddressIntegration uniqueAddressIntegration) {
+  public Sep31DepositInfoGeneratorApi(
+      UniqueAddressIntegration uniqueAddressIntegration,
+      PaymentObservingAccountsManager paymentObservingAccountsManager) {
     this.uniqueAddressIntegration = uniqueAddressIntegration;
+    this.paymentObservingAccountsManager = paymentObservingAccountsManager;
   }
 
   @Override
@@ -21,11 +27,19 @@ public class Sep31DepositInfoGeneratorApi implements Sep31DepositInfoGenerator {
     GetUniqueAddressResponse response = uniqueAddressIntegration.getUniqueAddress(txn.getId());
     GetUniqueAddressResponse.UniqueAddress uniqueAddress = response.getUniqueAddress();
 
+    if (uniqueAddress == null) {
+      throw new InternalServerErrorException(
+          "The server does not respond with a unique address for SEP31 deposit");
+    }
+
     if (!Objects.toString(uniqueAddress.getMemo(), "").isEmpty()
         && !Objects.toString(uniqueAddress.getMemoType(), "").isEmpty()) {
       // Check the validity of the returned memo and memo type
       MemoHelper.makeMemo(uniqueAddress.getMemo(), uniqueAddress.getMemoType());
     }
+
+    // Add to payment observer manager so that we get events of the stellar address.
+    paymentObservingAccountsManager.upsert(uniqueAddress.getStellarAddress(), true);
 
     return new Sep31DepositInfo(
         uniqueAddress.getStellarAddress(), uniqueAddress.getMemo(), uniqueAddress.getMemoType());
