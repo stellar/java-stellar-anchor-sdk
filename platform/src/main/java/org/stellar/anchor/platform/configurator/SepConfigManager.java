394048dc1b916ca3d809db0d121bcb60bdb80169
Jamie Li <lijamie@gmail.com>
2022-11-07 13:39:19 -0800

    quick fix.

--- platform/src/main/java/org/stellar/anchor/platform/configurator/SepConfigManager.java
+++ platform/src/main/java/org/stellar/anchor/platform/configurator/SepConfigManager.java
@@ -42,8 +42,11 @@ class SepServerConfigAdapter extends SpringConfigAdapter {
     copy(config, "sep_server.port", "server.port");
     set("spring.mvc.converters.preferred-json-mapper", "gson");
     if (config.getBoolean("metrics.enabled")) {
+      set("management.endpoints.enabled-by-default", true);
       copy(config, "sep_server.management_server_port", "server.port");
       set("management.endpoints.web.exposure.include", "health,info,prometheus");
+    } else {
+      set("management.endpoints.enabled-by-default", false);
     }
   }
 }
