30fccf8d04639ed7a995191066b976693eaec0ce
Jamie Li <lijamie@gmail.com>
2022-11-16 14:58:31 -0800

    add Sep1Service tests

--- core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
+++ core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
@@ -1,14 +1,16 @@
 package org.stellar.anchor.sep1;
 
-import static org.stellar.anchor.util.Log.debug;
-import static org.stellar.anchor.util.Log.debugF;
+import org.stellar.anchor.api.exception.InvalidConfigException;
+import org.stellar.anchor.config.Sep1Config;
+import org.stellar.anchor.util.Log;
+import org.stellar.anchor.util.NetUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.stellar.anchor.config.Sep1Config;
-import org.stellar.anchor.util.Log;
-import org.stellar.anchor.util.NetUtil;
+
+import static org.stellar.anchor.util.Log.debug;
+import static org.stellar.anchor.util.Log.debugF;
 
 public class Sep1Service {
   private String tomlValue;
@@ -18,7 +20,7 @@ public class Sep1Service {
    *
    * @param sep1Config The Sep1 configuration.
    */
-  public Sep1Service(Sep1Config sep1Config) throws IOException {
+  public Sep1Service(Sep1Config sep1Config) throws IOException, InvalidConfigException {
     if (sep1Config.isEnabled()) {
       debug("sep1Config:", sep1Config);
       switch (sep1Config.getType().toLowerCase()) {
@@ -34,6 +36,9 @@ public class Sep1Service {
           debugF("reading stellar.toml from {}", sep1Config.getValue());
           tomlValue = NetUtil.fetch(sep1Config.getValue());
           break;
+        default:
+          throw new InvalidConfigException(
+              String.format("invalid sep1.type: %s", sep1Config.getType()));
       }
 
       Log.info("Sep1Service initialized.");
--- core/src/test/kotlin/org/stellar/anchor/sep1/Sep1ServiceTest.kt
+++ core/src/test/kotlin/org/stellar/anchor/sep1/Sep1ServiceTest.kt
@@ -1,14 +1,74 @@
 package org.stellar.anchor.sep1
 
-import io.mockk.mockk
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import java.nio.file.Files
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
+import org.stellar.anchor.api.exception.InvalidConfigException
 import org.stellar.anchor.config.Sep1Config
+import org.stellar.anchor.util.NetUtil
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class Sep1ServiceTest {
+
+  @MockK(relaxed = true) lateinit var sep1Config: Sep1Config
+
+  @BeforeEach
+  fun setUp() {
+    MockKAnnotations.init(this, relaxUnitFun = true)
+  }
+
   @Test
-  fun `simple test if the toml file is read`() {
-    val sep1Config = mockk<Sep1Config>()
+  fun `disabled Sep1Service should return null string as toml value`() {
+    every { sep1Config.isEnabled } returns false
+    var sep1Service = Sep1Service(sep1Config)
+    assertEquals(null, sep1Service.stellarToml)
+  }
+
+  @Test
+  fun `test string type`() {
+    every { sep1Config.isEnabled } returns true
+    every { sep1Config.type } returns "string"
+    every { sep1Config.value } returns "toml content"
+    val sep1Service = Sep1Service(sep1Config)
+    assertEquals("toml content", sep1Service.stellarToml)
+  }
+
+  @Test
+  fun `test file type`() {
+    mockkStatic(Files::class)
+    every { Files.readString(any()) } returns "toml content"
+    every { sep1Config.isEnabled } returns true
+    every { sep1Config.type } returns "file"
+    every { sep1Config.value } returns "toml_file_path"
+
+    val sep1Service = Sep1Service(sep1Config)
+    assertEquals("toml content", sep1Service.stellarToml)
+  }
+
+  @Test
+  fun `test url type`() {
+    mockkStatic(NetUtil::class)
+    every { NetUtil.fetch(any()) } returns "toml content"
+    every { sep1Config.isEnabled } returns true
+    every { sep1Config.type } returns "url"
+    every { sep1Config.value } returns "toml_file_path"
+
+    val sep1Service = Sep1Service(sep1Config)
+    assertEquals("toml content", sep1Service.stellarToml)
+  }
+
+  @Test
+  fun `test bad type`() {
+    every { sep1Config.isEnabled } returns true
+    every { sep1Config.type } returns "bad"
+    every { sep1Config.value } returns "toml_file_path"
+
+    assertThrows<InvalidConfigException> { Sep1Service(sep1Config) }
   }
 }
