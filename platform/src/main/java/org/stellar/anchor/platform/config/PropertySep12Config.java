d5620e90d812538de29720bd08c513adc9b08499
Jamie Li <lijamie@gmail.com>
2022-11-23 00:57:26 -0800

    Clean up PropertiesSep12Config and remove the validator interface.

--- platform/src/main/java/org/stellar/anchor/platform/config/PropertySep12Config.java
+++ platform/src/main/java/org/stellar/anchor/platform/config/PropertySep12Config.java
@@ -1,27 +1,14 @@
 package org.stellar.anchor.platform.config;
 
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 import org.stellar.anchor.config.Sep12Config;
 
 @Data
-public class PropertySep12Config implements Sep12Config, Validator {
+public class PropertySep12Config implements Sep12Config {
   Boolean enabled;
   String customerIntegrationEndPoint;
 
   public PropertySep12Config(CallbackApiConfig callbackApiConfig) {
     this.customerIntegrationEndPoint = callbackApiConfig.getBaseUrl();
-  }
-
-  @Override
-  public boolean supports(@NotNull Class<?> clazz) {
-    return Sep12Config.class.isAssignableFrom(clazz);
-  }
-
-  @Override
-  public void validate(@NotNull Object target, @NotNull Errors errors) {
-    Sep12Config config = (Sep12Config) target;
   }
 }
