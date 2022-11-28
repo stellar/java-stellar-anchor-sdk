d5ccbf29892c040187bb19ed52e36414c04571f5
Jamie Li <lijamie@gmail.com>
2022-11-28 14:53:28 +0800

    Add Sep24Config validation and tests

--- platform/src/main/java/org/stellar/anchor/platform/config/PropertySep24Config.java
+++ platform/src/main/java/org/stellar/anchor/platform/config/PropertySep24Config.java
@@ -1,10 +1,13 @@
 package org.stellar.anchor.platform.config;
 
+import static org.stellar.anchor.util.StringHelper.isEmpty;
+
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import org.stellar.anchor.config.Sep24Config;
+import org.stellar.anchor.util.NetUtil;
 
 @Data
 public class PropertySep24Config implements Sep24Config, Validator {
@@ -19,6 +22,26 @@ public class PropertySep24Config implements Sep24Config, Validator {
 
   @Override
   public void validate(@NotNull Object target, @NotNull Errors errors) {
-    Sep24Config config = (Sep24Config) target;
+    PropertySep24Config config = (PropertySep24Config) target;
+    if (isEmpty(config.getInteractiveUrl())) {
+      errors.rejectValue(
+          "interactiveUrl",
+          "sep24-interactive-url-invalid",
+          "sep24.interactive_url is not defined.");
+    } else if (!NetUtil.isUrlValid(config.getInteractiveUrl())) {
+      errors.rejectValue(
+          "interactiveUrl",
+          "sep24-interactive-url-invalid",
+          String.format("sep24.interactive_url:%s is not valid", config.getInteractiveUrl()));
+    }
+
+    if (config.getInteractiveJwtExpiration() <= 0) {
+      errors.rejectValue(
+          "interactiveJwtExpiration",
+          "sep24-interactive-jwt-expiration-invalid",
+          String.format(
+              "sep24.interactive_jwt_expiration:%s is not valid",
+              config.getInteractiveJwtExpiration()));
+    }
   }
 }
