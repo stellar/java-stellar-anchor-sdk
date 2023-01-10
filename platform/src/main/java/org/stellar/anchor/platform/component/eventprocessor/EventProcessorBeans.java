5a2373c74511217be3d75c6245dcb5409e3a67c6
Jamie Li <lijamie@gmail.com>
2023-01-10 12:06:15 +0800

    Add EventProcessorConfig

--- platform/src/main/java/org/stellar/anchor/platform/component/eventprocessor/EventProcessorBeans.java
+++ platform/src/main/java/org/stellar/anchor/platform/component/eventprocessor/EventProcessorBeans.java
@@ -2,12 +2,13 @@ package org.stellar.anchor.platform.component.eventprocessor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.stellar.anchor.platform.config.EventProcessorConfig;
 import org.stellar.anchor.platform.event.EventProcessor;
 
 @Configuration
 public class EventProcessorBeans {
   @Bean
-  EventProcessor eventProcessor() {
+  EventProcessor eventProcessor(EventProcessorConfig config) {
     return new EventProcessor();
   }
 }
