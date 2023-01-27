a84aeb0fca145c1e93730d4815bd34a1ff994a1d
Gleb <gleb@stellar.org>
2023-01-27 11:32:05 -0800

    Fix import

--- kotlin-reference-server/src/main/kotlin/com/example/ReferenceServer.kt
+++ kotlin-reference-server/src/main/kotlin/com/example/ReferenceServer.kt
@@ -9,11 +9,13 @@ import com.sksamuel.hoplite.ConfigLoaderBuilder
 import com.sksamuel.hoplite.PropertySource
 import com.sksamuel.hoplite.addFileSource
 import com.sksamuel.hoplite.addResourceSource
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import mu.KotlinLogging
 
