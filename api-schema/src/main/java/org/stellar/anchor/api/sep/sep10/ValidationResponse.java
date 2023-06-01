f140a0f8ddc73d808a3e1f5178650208fbbfa593
Jamie Li <lijamie@gmail.com>
2023-06-01 10:48:18 -0700

    Add SEP comments

--- api-schema/src/main/java/org/stellar/anchor/api/sep/sep10/ValidationResponse.java
+++ api-schema/src/main/java/org/stellar/anchor/api/sep/sep10/ValidationResponse.java
@@ -2,6 +2,12 @@ package org.stellar.anchor.api.sep.sep10;
 
 import lombok.Data;
 
+/**
+ * The response body of the validation of the SEP-10 authentication flow.
+ *
+ * @see <a
+ *     href="https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0010.md">SEP-10</a>
+ */
 @SuppressWarnings("unused")
 @Data
 public class ValidationResponse {
--- api-schema/src/main/java/org/stellar/anchor/api/sep/sep12/PutCustomerVerificationResponse.java
+++ api-schema/src/main/java/org/stellar/anchor/api/sep/sep12/PutCustomerVerificationResponse.java
@@ -1,3 +1,10 @@
 package org.stellar.anchor.api.sep.sep12;
 
+/**
+ * The response to the PUT /customer/verification endpoint of SEP-12.
+ *
+ * @see <a
+ *     href="https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0012.md#fields">Refer
+ *     to SEP-12</a>
+ */
 public class PutCustomerVerificationResponse extends Sep12GetCustomerResponse {}
