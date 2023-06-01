463641b553ff7b1946ae2bbce37efaa1d5c0ca91
Jamie Li <lijamie@gmail.com>
2023-06-01 08:46:26 -0700

    more changes

--- api-schema/src/main/java/org/stellar/anchor/api/platform/PlatformTransactionData.java
+++ api-schema/src/main/java/org/stellar/anchor/api/platform/PlatformTransactionData.java
@@ -11,6 +11,14 @@ import lombok.experimental.SuperBuilder;
 import org.stellar.anchor.api.sep.SepTransactionStatus;
 import org.stellar.anchor.api.shared.*;
 
+/**
+ * The transaction data in the response body of the GET /transactions/{id} endpoint of the Platform
+ * API.
+ *
+ * @see <a
+ *     href="https://github.com/stellar/stellar-docs/blob/main/openapi/ap/Platform%20API.yml">Platform
+ *     API</a>
+ */
 @Getter
 @Setter
 @SuperBuilder
