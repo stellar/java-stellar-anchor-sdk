e3f77e3333c2de29d8e3949ddf3f09848f278a7d
imazur <imazur@softserveinc.com>
2023-06-07 21:57:00 +0300

    SDFSOF-55 - Added a memo_type validation for deposit case when integration with Fireblocks is enabled

--- core/src/main/java/org/stellar/anchor/config/CustodyConfig.java
+++ core/src/main/java/org/stellar/anchor/config/CustodyConfig.java
@@ -3,6 +3,7 @@ package org.stellar.anchor.config;
 public interface CustodyConfig {
 
   String NONE_CUSTODY_TYPE = "none";
+  String FIREBLOCKS_CUSTODY_TYPE = "fireblocks";
 
   default boolean isCustodyIntegrationEnabled() {
     return !NONE_CUSTODY_TYPE.equals(getType());
