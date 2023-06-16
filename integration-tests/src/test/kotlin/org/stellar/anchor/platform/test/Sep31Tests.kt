9c796c0c697d567c012430311cb6843e3bbab1bf
Philip Liu <12836897+philipliu@users.noreply.github.com>
2023-06-16 13:31:03 -0400

    [ANCHOR-343] Disable SEP-31 and SEP-38 for native asset (#950)
    * Disable SEP-31 and SEP-38 for native asset
    
    * Fix some unit tests

--- integration-tests/src/test/kotlin/org/stellar/anchor/platform/test/Sep31Tests.kt
+++ integration-tests/src/test/kotlin/org/stellar/anchor/platform/test/Sep31Tests.kt
@@ -511,59 +511,6 @@ private const val expectedSep31Info =
             }
           }
         }
-      },
-      "native": {
-        "enabled": true,
-        "quotes_supported": true,
-        "quotes_required": true,
-        "fee_fixed": 0,
-        "fee_percent": 0,
-        "max_amount": 1000000,
-        "sep12": {
-          "sender": {
-            "types": {
-              "sep31-sender": {
-                "description": "U.S. citizens limited to sending payments of less than ${'$'}10,000 in value"
-              },
-              "sep31-large-sender": {
-                "description": "U.S. citizens that do not have sending limits"
-              },
-              "sep31-foreign-sender": {
-                "description": "non-U.S. citizens sending payments of less than ${'$'}10,000 in value"
-              }
-            }
-          },
-          "receiver": {
-            "types": {
-              "sep31-receiver": {
-                "description": "U.S. citizens receiving USD"
-              },
-              "sep31-foreign-receiver": {
-                "description": "non-U.S. citizens receiving USD"
-              }
-            }
-          }
-        },
-        "fields": {
-          "transaction": {
-            "receiver_routing_number": {
-              "description": "routing number of the destination bank account",
-              "optional": false
-            },
-            "receiver_account_number": {
-              "description": "bank account number of the destination",
-              "optional": false
-            },
-            "type": {
-              "description": "type of deposit to make",
-              "choices": [
-                "SEPA",
-                "SWIFT"
-              ],
-              "optional": false
-            }
-          }
-        }
       }
     }
   }
