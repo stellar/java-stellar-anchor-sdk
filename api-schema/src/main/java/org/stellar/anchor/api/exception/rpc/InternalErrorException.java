4e2dd5b117cb201bca39988e2ff9a7eb79798445
MazurakIhor <131388095+MazurakIhor@users.noreply.github.com>
2023-07-01 00:20:42 +0300

    SDFSOF-63 - Added implementation of endpoint for handling RPC calls with corresponding request validation and basic error handling (#968)
    SDFSOF-63 - Added implementation of endpoint for handling RPC calls with corresponding request validation and basic error handling
    
    What
    Implementation of endpoint for handling RPC calls with request validation and error handling
    
    Why
    This endpoint will handle RPC calls that should replace PATCH /transactions REST endpoint

---
+++ api-schema/src/main/java/org/stellar/anchor/api/exception/rpc/InternalErrorException.java
@@ -0,0 +1,14 @@
+package org.stellar.anchor.api.exception.rpc;
+
+import static org.stellar.anchor.api.rpc.RpcErrorCode.INTERNAL_ERROR;
+
+import lombok.EqualsAndHashCode;
+import org.stellar.anchor.api.exception.RpcException;
+
+/** Internal error occurred during rpc call processing. */
+@EqualsAndHashCode(callSuper = false)
+public class InternalErrorException extends RpcException {
+  public InternalErrorException(String message) {
+    super(INTERNAL_ERROR.getErrorCode(), message);
+  }
+}
