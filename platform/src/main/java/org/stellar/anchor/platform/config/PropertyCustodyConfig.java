e1834cbbe6c7a694602226186938f5b8d9baaf16
rkharevych <rkhar@softserveinc.com>
2023-07-11 12:35:26 +0300

    Add trustline config

--- platform/src/main/java/org/stellar/anchor/platform/config/PropertyCustodyConfig.java
+++ platform/src/main/java/org/stellar/anchor/platform/config/PropertyCustodyConfig.java
@@ -2,8 +2,11 @@ package org.stellar.anchor.platform.config;
 
 import static org.stellar.anchor.util.StringHelper.isEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import org.stellar.anchor.config.CustodyConfig;
@@ -13,6 +16,7 @@ public class PropertyCustodyConfig implements CustodyConfig, Validator {
 
   private String type;
   private HttpClientConfig httpClient;
+  private TrustLine trustLine;
 
   @Override
   public boolean supports(@NotNull Class<?> clazz) {
@@ -22,8 +26,10 @@ public class PropertyCustodyConfig implements CustodyConfig, Validator {
   @Override
   public void validate(@NotNull Object target, @NotNull Errors errors) {
     validateType(errors);
-    if (this.isCustodyIntegrationEnabled()) {
+    if (isCustodyIntegrationEnabled()) {
       httpClient.validate("custody", errors);
+      validateCheckCronExpression(errors);
+      validateCheckDuration(errors);
     }
   }
 
@@ -32,5 +38,35 @@ public class PropertyCustodyConfig implements CustodyConfig, Validator {
       errors.rejectValue(
           "type", "custody-type-empty", "The custody.type cannot be empty and must be defined");
     }
+  }
+
+  private void validateCheckCronExpression(Errors errors) {
+    if (isEmpty(trustLine.checkCronExpression)) {
+      errors.reject(
+          "custody-trust_line-check_cron_expression-empty",
+          "The custody.trust_line.check_cron_expression is empty");
+    }
+    if (!CronExpression.isValidExpression(trustLine.checkCronExpression)) {
+      errors.reject(
+          "custody-trust_line-check_cron_expression-invalid",
+          "The custody.trust_line.check_cron_expression is invalid");
+    }
+  }
+
+  private void validateCheckDuration(Errors errors) {
+    if (trustLine.checkDuration <= 0) {
+      errors.reject(
+          "custody-trust_line-check_duration-invalid",
+          "trust_line-check_duration must be greater than 0");
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class TrustLine {
+    private String checkCronExpression;
+    private int checkDuration;
+    private String timeoutMessage;
   }
 }
--- platform/src/test/kotlin/org/stellar/anchor/platform/config/CustodyConfigTest.kt
+++ platform/src/test/kotlin/org/stellar/anchor/platform/config/CustodyConfigTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.NullSource
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.validation.BindException
 import org.springframework.validation.Errors
+import org.stellar.anchor.platform.config.PropertyCustodyConfig.TrustLine
 
 class PropertyCustodyConfigTest {
 
@@ -18,6 +19,7 @@ class PropertyCustodyConfigTest {
     config = PropertyCustodyConfig()
     config.type = "custodyType"
     config.httpClient = HttpClientConfig(10, 30, 30, 60)
+    config.trustLine = TrustLine("* * * * * *", 10, "testMessage")
     errors = BindException(config, "config")
   }
 
@@ -110,6 +112,55 @@ class PropertyCustodyConfigTest {
     config.httpClient.readTimeout = timeout
     config.httpClient.writeTimeout = timeout
     config.httpClient.callTimeout = timeout
+    config.validate(config, errors)
+    assertFalse(errors.hasErrors())
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["* * * * * *", "0 0/15 * * * *"])
+  fun `test valid trust_line_check_cron`(cron: String) {
+    config.trustLine.checkCronExpression = cron
+    config.validate(config, errors)
+    assertFalse(errors.hasErrors())
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = [""])
+  fun `test empty trust_line_check_cron`(cron: String) {
+    config.trustLine.checkCronExpression = cron
+    config.validate(config, errors)
+    assertErrorCode(errors, "custody-trust_line-check_cron_expression-empty")
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["* * * * *", "* * * * * * *", "0/a * * * * *"])
+  fun `test invalid trust_line_check_cron`(cron: String) {
+    config.trustLine.checkCronExpression = cron
+    config.validate(config, errors)
+    assertErrorCode(errors, "custody-trust_line-check_cron_expression-invalid")
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = [1, Int.MAX_VALUE])
+  fun `test valid trust_line_check_duration`(duration: Int) {
+    config.trustLine.checkDuration = duration
+    config.validate(config, errors)
+    assertFalse(errors.hasErrors())
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = [0, -1, Int.MIN_VALUE])
+  fun `test invalid trust_line_check_duration`(duration: Int) {
+    config.trustLine.checkDuration = duration
+    config.validate(config, errors)
+    assertErrorCode(errors, "custody-trust_line-check_duration-invalid")
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = [-1, Int.MIN_VALUE])
+  fun `test invalid trust_line none type`(timeout: Int) {
+    config.type = "none"
+    config.trustLine.checkDuration = timeout
     config.validate(config, errors)
     assertFalse(errors.hasErrors())
   }
