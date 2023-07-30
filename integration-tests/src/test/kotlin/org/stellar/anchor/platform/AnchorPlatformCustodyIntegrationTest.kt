73d220665003ebc9ccf507fd50d26f2644479917
imazur <imazur@softserveinc.com>
2023-07-30 20:41:34 +0300

    Change test order

--- integration-tests/src/test/kotlin/org/stellar/anchor/platform/AnchorPlatformCustodyIntegrationTest.kt
+++ integration-tests/src/test/kotlin/org/stellar/anchor/platform/AnchorPlatformCustodyIntegrationTest.kt
@@ -1,36 +1,36 @@
-// package org.stellar.anchor.platform
-//
-// import kotlinx.coroutines.*
-// import okhttp3.mockwebserver.MockWebServer
-// import org.junit.jupiter.api.*
-// import org.stellar.anchor.platform.test.*
-//
-// @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
-// class AnchorPlatformCustodyIntegrationTest :
-//  AbstractIntegrationTest(TestConfig(testProfileName = "default-custody")) {
-//  companion object {
-//    private val singleton = AnchorPlatformCustodyIntegrationTest()
-//    private val custodyMockServer = MockWebServer()
-//
-//    @BeforeAll
-//    @JvmStatic
-//    fun construct() {
-//      custodyMockServer.start()
-//      val mockServerUrl = custodyMockServer.url("").toString()
-//      singleton.setUp(mapOf("custody.fireblocks.base_url" to mockServerUrl))
-//    }
-//
-//    @AfterAll
-//    @JvmStatic
-//    fun destroy() {
-//      custodyMockServer.shutdown()
-//      singleton.tearDown()
-//    }
-//  }
-//
-//  @Test
-//  @Order(1)
-//  fun runCustodyApiTest() {
-//    singleton.custodyApiTests.testAll(custodyMockServer)
-//  }
-// }
+package org.stellar.anchor.platform
+
+import kotlinx.coroutines.*
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.*
+import org.stellar.anchor.platform.test.*
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class AnchorPlatformCustodyIntegrationTest :
+  AbstractIntegrationTest(TestConfig(testProfileName = "default-custody")) {
+  companion object {
+    private val singleton = AnchorPlatformCustodyIntegrationTest()
+    private val custodyMockServer = MockWebServer()
+
+    @BeforeAll
+    @JvmStatic
+    fun construct() {
+      custodyMockServer.start()
+      val mockServerUrl = custodyMockServer.url("").toString()
+      singleton.setUp(mapOf("custody.fireblocks.base_url" to mockServerUrl))
+    }
+
+    @AfterAll
+    @JvmStatic
+    fun destroy() {
+      custodyMockServer.shutdown()
+      singleton.tearDown()
+    }
+  }
+
+  @Test
+  @Order(70)
+  fun runCustodyApiTest() {
+    singleton.custodyApiTests.testAll(custodyMockServer)
+  }
+}
