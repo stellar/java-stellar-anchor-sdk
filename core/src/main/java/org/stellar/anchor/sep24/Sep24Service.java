95f9b379ed32e1fd7dc48076f37e8b6f10a25d69
philipliu <philip.liu@stellar.org>
2023-08-01 13:13:54 -0400

    Fix some logs

--- core/src/main/java/org/stellar/anchor/sep24/Sep24Service.java
+++ core/src/main/java/org/stellar/anchor/sep24/Sep24Service.java
@@ -95,7 +95,7 @@ public class Sep24Service {
     String strAmount = withdrawRequest.get("amount");
 
     String lang = validateLanguage(appConfig, withdrawRequest.get("lang"));
-    debug("language: {}", lang);
+    debugF("language: {}", lang);
 
     if (assetCode == null) {
       info("missing 'asset_code'");
@@ -232,7 +232,7 @@ public class Sep24Service {
     }
 
     String lang = validateLanguage(appConfig, depositRequest.get("lang"));
-    debug("language: {}", lang);
+    debugF("language: {}", lang);
 
     if (assetCode == null) {
       info("missing 'asset_code'");
