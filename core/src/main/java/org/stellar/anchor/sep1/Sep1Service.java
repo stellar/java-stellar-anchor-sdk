8da260c3cb20298cfba848de5d2311f8f91e0d95
Reece Markowsky <reece@stellar.org>
2023-08-09 16:29:53 -0700

    revert

--- core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
+++ core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
@@ -1,16 +1,16 @@
 package org.stellar.anchor.sep1;
 
-import static org.stellar.anchor.util.Log.debugF;
+import static org.stellar.anchor.util.Log.debug;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import org.stellar.anchor.api.exception.InvalidConfigException;
 import org.stellar.anchor.config.Sep1Config;
 import org.stellar.anchor.util.FileUtil;
+import org.stellar.anchor.util.Log;
 import org.stellar.anchor.util.NetUtil;
 
 public class Sep1Service {
-
   private String tomlValue;
 
   /**
@@ -22,25 +22,26 @@ public class Sep1Service {
    */
   public Sep1Service(Sep1Config sep1Config) throws IOException, InvalidConfigException {
     if (sep1Config.isEnabled()) {
-      this.tomlValue = handleConfigType(sep1Config);
-    }
-  }
+      debug("sep1Config:", sep1Config);
+      switch (sep1Config.getType()) {
+        case STRING:
+          debug("reading stellar.toml from config[sep1.toml.value]");
+          tomlValue = sep1Config.getValue();
+          break;
+        case FILE:
+          debugF("reading stellar.toml from {}", sep1Config.getValue());
+          tomlValue = FileUtil.read(Path.of(sep1Config.getValue()));
+          break;
+        case URL:
+          debugF("reading stellar.toml from {}", sep1Config.getValue());
+          tomlValue = NetUtil.fetch(sep1Config.getValue());
+          break;
+        default:
+          throw new InvalidConfigException(
+              String.format("invalid sep1.type: %s", sep1Config.getType()));
+      }
 
-  private String handleConfigType(Sep1Config sep1Config)
-      throws IOException, InvalidConfigException {
-    switch (sep1Config.getType()) {
-      case STRING:
-        debugF("reading stellar.toml from config[sep1.toml.value]");
-        return sep1Config.getValue();
-      case FILE:
-        debugF("reading stellar.toml from {}", sep1Config.getValue());
-        return FileUtil.read(Path.of(sep1Config.getValue()));
-      case URL:
-        debugF("reading stellar.toml from {}", sep1Config.getValue());
-        return NetUtil.fetch(sep1Config.getValue());
-      default:
-        throw new InvalidConfigException(
-            String.format("invalid sep1.type: %s", sep1Config.getType()));
+      Log.info("Sep1Service initialized.");
     }
   }
 
