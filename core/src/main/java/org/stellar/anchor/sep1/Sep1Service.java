3904048b8cde9f7c989ae6cb2fec9d112307de52
Reece Markowsky <reece@stellar.org>
2023-08-11 11:56:52 -0700

    ANCHOR-403 Obfuscate Exception in Sep1Helper instead of NetUtil

--- core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
+++ core/src/main/java/org/stellar/anchor/sep1/Sep1Service.java
@@ -1,6 +1,5 @@
 package org.stellar.anchor.sep1;
 
-import static org.stellar.anchor.util.Log.debug;
 import static org.stellar.anchor.util.Log.debugF;
 
 import java.io.IOException;
@@ -23,10 +22,9 @@ public class Sep1Service {
    */
   public Sep1Service(Sep1Config sep1Config) throws IOException, InvalidConfigException {
     if (sep1Config.isEnabled()) {
-      debug("sep1Config:", sep1Config);
       switch (sep1Config.getType()) {
         case STRING:
-          debug("reading stellar.toml from config[sep1.toml.value]");
+          debugF("reading stellar.toml from config[sep1.toml.value]");
           tomlValue = sep1Config.getValue();
           break;
         case FILE:
