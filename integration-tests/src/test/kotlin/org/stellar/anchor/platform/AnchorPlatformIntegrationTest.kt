02a4138c861a04db2a35999b3dd5f7a5429fb9e4
Jamie Li <lijamie@gmail.com>
2023-08-29 10:44:55 -0700

    Removed sep24 test profile

--- integration-tests/src/test/kotlin/org/stellar/anchor/platform/AnchorPlatformIntegrationTest.kt
+++ integration-tests/src/test/kotlin/org/stellar/anchor/platform/AnchorPlatformIntegrationTest.kt
@@ -6,7 +6,7 @@ import org.stellar.anchor.platform.test.*
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class AnchorPlatformIntegrationTest :
-  AbstractIntegrationTest(TestConfig(testProfileName = "sep24")) {
+  AbstractIntegrationTest(TestConfig(testProfileName = "default")) {
   companion object {
     private val singleton = AnchorPlatformIntegrationTest()
 
