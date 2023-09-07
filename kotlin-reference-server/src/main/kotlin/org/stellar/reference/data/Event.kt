abee012542df48034c84764e7d7702fcff97b34d
Jake Urban <jake@stellar.org>
2023-09-06 20:01:38 -0700

    testing using send event request payload kotlin class

--- kotlin-reference-server/src/main/kotlin/org/stellar/reference/data/Event.kt
+++ kotlin-reference-server/src/main/kotlin/org/stellar/reference/data/Event.kt
@@ -3,14 +3,14 @@ package org.stellar.reference.data
 import org.stellar.anchor.api.platform.GetQuoteResponse
 import org.stellar.anchor.api.platform.GetTransactionResponse
 
-public data class SendEventRequest(
+data class SendEventRequest(
   val id: String,
   val type: String,
   val timestamp: String,
   val payload: SendEventRequestPayload
 )
 
-public data class SendEventRequestPayload(
+data class SendEventRequestPayload(
   val transaction: GetTransactionResponse,
   val quote: GetQuoteResponse
 )
