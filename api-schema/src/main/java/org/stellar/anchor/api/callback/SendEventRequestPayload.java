471b63202724f7f9fba30393d7c1ee7e68a5697d
Jake Urban <10968980+JakeUrban@users.noreply.github.com>
2023-09-07 13:01:18 -0700

    [ANCHOR-435]: Update event callback schema (#1095)

---
+++ api-schema/src/main/java/org/stellar/anchor/api/callback/SendEventRequestPayload.java
@@ -0,0 +1,37 @@
+package org.stellar.anchor.api.callback;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.stellar.anchor.api.event.AnchorEvent;
+import org.stellar.anchor.api.platform.GetQuoteResponse;
+import org.stellar.anchor.api.platform.GetTransactionResponse;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendEventRequestPayload {
+  GetTransactionResponse transaction;
+  GetQuoteResponse quote;
+
+  /**
+   * Creates a SendEventRequestPayload from an AnchorEvent.
+   *
+   * @param event
+   * @return a SendEventRequestPayload
+   */
+  public static SendEventRequestPayload from(AnchorEvent event) {
+    SendEventRequestPayload payload = new SendEventRequestPayload();
+    switch (event.getType()) {
+      case QUOTE_CREATED:
+        payload.setQuote(event.getQuote());
+      case TRANSACTION_CREATED:
+        payload.setTransaction(event.getTransaction());
+      case TRANSACTION_ERROR:
+        payload.setTransaction(event.getTransaction());
+      case TRANSACTION_STATUS_CHANGED:
+        payload.setTransaction(event.getTransaction());
+    }
+    return payload;
+  }
+}
