34e04ed90deee2c3ae6ce98d76ec1e815317e7b3
Gleb <gleb@stellar.org>
2023-09-13 10:03:11 -0700

    Add javadoc

--- core/src/main/java/org/stellar/anchor/util/ConfigHelper.java
+++ core/src/main/java/org/stellar/anchor/util/ConfigHelper.java
@@ -7,6 +7,16 @@ import org.stellar.anchor.config.ClientsConfig;
 import org.stellar.anchor.sep24.Sep24Transaction;
 
 public final class ConfigHelper {
+  /**
+   * Gets client config from the config collection that matches the predicate. If clientDomain is
+   * provided, lookup in the collection by the domain. If it's not provided, lookup by the SEP-10
+   * account.
+   *
+   * @param clientsConfig collection of client configurations
+   * @param clientDomain domain to use for the lookup
+   * @param sep10Account SEP-10 authenticated account to use for lookup
+   * @return client config by the predicate. If not found, returns null.
+   */
   public static ClientsConfig.ClientConfig getClientConfig(
       ClientsConfig clientsConfig, String clientDomain, String sep10Account)
       throws SepValidationException {
