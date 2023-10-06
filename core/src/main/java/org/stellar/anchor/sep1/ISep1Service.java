fc7e92e95bd60ae6f9caa3aafa76f03caa6833d6
Jamie Li <lijamie@gmail.com>
2023-10-06 09:29:26 -0700

    Add class comments

--- core/src/main/java/org/stellar/anchor/sep1/ISep1Service.java
+++ core/src/main/java/org/stellar/anchor/sep1/ISep1Service.java
@@ -2,6 +2,10 @@ package org.stellar.anchor.sep1;
 
 import org.stellar.anchor.config.Sep1Config;
 
+/**
+ * The interface for SEP-1 service. Use this interface to override the default implementation of the
+ * Sep1Service class.
+ */
 public interface ISep1Service {
   /**
    * Read the stellar.toml content from Java resource. Override this method to read from a different
