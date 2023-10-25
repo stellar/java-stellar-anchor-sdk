4f2d8a7fca076baba82da5ae809b9bb4b1112339
Jamie Li <lijamie@gmail.com>
2023-10-25 09:28:09 -0700

    Extract the test concurrency settings to `scripts.gradle.kts` file (#1173)
    ### Description
    Extract the test concurrency settings to `scripts.gradle.kts` file

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -55,29 +55,12 @@ dependencies {
   testImplementation(libs.okhttp3.tls)
 }
 
-tasks.test {
-  // Enable parallel test execution
-  systemProperty("junit.jupiter.execution.parallel.enabled", true)
-  // Enable parallel test execution
-  systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_method")
-  // Allocate thread count based on available processors
-  systemProperty("junit.jupiter.execution.parallel.config.strategy", "dynamic")
-  // Set default parallel mode to same thread. All tests within a class are run in sequence.
-  systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
-  // Set default parallel mode for classes to concurrent. All test classes are run in parallel.
-  systemProperty("junit.jupiter.execution.parallel.mode.classes.default", "concurrent")
+apply(from = "$rootDir/scripts.gradle.kts")
+@Suppress("UNCHECKED_CAST")
+val enableTestConcurrency = extra["enableTestConcurrency"] as (Test) -> Unit
 
-  // Set default test class order to order annotation. All test classes are run in parallel.
-  // Some tests take longer to run. Enabling the order will execute long-running tests first to
-  // shorten the overall test time.
-  systemProperty(
-    "junit.jupiter.testclass.order.default",
-    "org.junit.jupiter.api.ClassOrderer\$OrderAnnotation"
-  )
-  maxParallelForks =
-    (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1).also {
-      println("junit5 ... setting maxParallelForks to $it")
-    }
+tasks.test {
+  enableTestConcurrency(this)
   testLogging {
     exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     events = setOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED)
