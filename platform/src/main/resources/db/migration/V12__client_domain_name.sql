5d7326fc0534e141d4c7d97440c6cf230e8fb43b
Philip Liu <12836897+philipliu@users.noreply.github.com>
2023-11-29 21:49:42 -0500

    [ANCHOR-555] Add `client_domain` and `client_name` to Platform transactions (#1210)
    ### Description
    
    This adds the `client_domain` and `client_name` to the Platform
    transaction objects
    
    ### Context
    
    `client_name` and `client_domain` are inaccessible to the business
    servers for SEP-6 and SEP-31 at the moment.
    
    ### Testing
    
    - `./gradlew test`
    
    ### Documentation
    
    The AP docs need to be updated with this new field.
    
    ### Known limitations
    
    N/A

---
+++ platform/src/main/resources/db/migration/V12__client_domain_name.sql
@@ -0,0 +1,5 @@
+ALTER TABLE sep6_transaction ADD client_domain VARCHAR(255);
+ALTER TABLE sep6_transaction ADD client_name VARCHAR(255);
+
+ALTER TABLE sep24_transaction ADD client_name VARCHAR(255);
+ALTER TABLE sep31_transaction ADD client_name VARCHAR(255);
