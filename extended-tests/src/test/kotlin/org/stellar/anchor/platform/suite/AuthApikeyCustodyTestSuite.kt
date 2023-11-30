34a555096ea66e6d4a447efd79971927b90a8fe8
Jamie Li <lijamie@gmail.com>
2023-11-30 22:21:11 +0800

    Create tests suites for different configurations

---
+++ extended-tests/src/test/kotlin/org/stellar/anchor/platform/suite/AuthApikeyCustodyTestSuite.kt
@@ -0,0 +1,8 @@
+package org.stellar.anchor.platform.suite
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.stellar.anchor.platform.extendedtest.auth.apikey.custody")
+class AuthApikeyCustodyTestSuite {}
