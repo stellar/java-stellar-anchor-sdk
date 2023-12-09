1adca496a2f37d312899325c732b7c66a525bc7d
Jamie Li <lijamie@gmail.com>
2023-12-09 12:39:32 +0800

    wip

--- service-runner/build.gradle.kts
+++ service-runner/build.gradle.kts
@@ -1,3 +1,8 @@
+import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerLogsContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+
 @Suppress(
   // The alias call in plugins scope produces IntelliJ false error which is suppressed here.
   "DSL_SCOPE_VIOLATION"
@@ -7,6 +12,7 @@ plugins {
   alias(libs.plugins.spring.boot)
   alias(libs.plugins.spring.dependency.management)
   alias(libs.plugins.kotlin.jvm)
+  id("com.bmuschko.docker-remote-api") version "9.3.7"
 }
 
 dependencies {
@@ -32,17 +38,11 @@ dependencies {
   implementation(project(":wallet-reference-server"))
 }
 
-tasks {
-  bootJar {
-    archiveBaseName.set("anchor-platform-runner")
-  }
-}
+tasks { bootJar { archiveBaseName.set("anchor-platform-runner") } }
 
 application { mainClass.set("org.stellar.anchor.platform.ServiceRunner") }
 
-/**
- * Start all the servers based on the `default` test configuration.
- */
+/** Start all the servers based on the `default` test configuration. */
 tasks.register<JavaExec>("startAllServers") {
   println("Starting all servers based on the `default` test configuration.")
   group = "application"
@@ -51,21 +51,65 @@ tasks.register<JavaExec>("startAllServers") {
 }
 
 /**
- * Start all the servers based on the test configuration specified by the TEST_PROFILE_NAME envionrment variable.
+ * Start all the servers based on the test configuration specified by the TEST_PROFILE_NAME
+ * envionrment variable.
  */
 tasks.register<JavaExec>("startServersWithTestProfile") {
-  println("Starting the servers based on the test configuration specified by the TEST_PROFILE_NAME envionrment variable.")
+  println(
+    "Starting the servers based on the test configuration specified by the TEST_PROFILE_NAME envionrment variable."
+  )
   group = "application"
   classpath = sourceSets["main"].runtimeClasspath
   mainClass.set("org.stellar.anchor.platform.run_profiles.RunTestProfile")
 }
 
-/**
- * Run docker-compose up to start Postgres, Kafka, Zookeeper,etc.
- */
+/** Run docker-compose up to start Postgres, Kafka, Zookeeper,etc. */
 tasks.register<JavaExec>("dockerComposeUp") {
   println("Running docker-compose up to start Postgres, Kafka, Zookeeper,etc.")
   group = "application"
   classpath = sourceSets["main"].runtimeClasspath
   mainClass.set("org.stellar.anchor.platform.run_profiles.RunDockerDevStack")
 }
+
+val pullImage by tasks.creating(DockerPullImage::class) { image.set("stellar/anchor-tests:v0.6.9") }
+
+val createContainer by
+  tasks.creating(DockerCreateContainer::class) {
+    dependsOn(pullImage)
+    targetImageId { pullImage.image.get() }
+    val homeDomain = System.getenv("HOME_DOMAIN") ?: "http://host.docker.internal:8080"
+    println("HOME_DOMAIN=$homeDomain")
+    println("PROJECT_DIR=${project.projectDir}")
+    val configPath = "${project.projectDir}/../platform/src/test/resources"
+    //      hostConfig.autoRemove.set(true)
+    hostConfig.binds.set(mutableMapOf(configPath to "/config"))
+    val logConfig = DockerCreateContainer.HostConfig.LogConfig()
+    hostConfig.logConfig("json-file", mapOf("max-size" to "100m"))
+    hostConfig.network.set("host")
+    cmd.set(
+      listOf(
+        "--home-domain",
+        "http://host.docker.internal:8080",
+        "--seps",
+        "1",
+        "10",
+        "--sep-config",
+        "//config/stellar-anchor-tests-sep-config.json",
+        "--verbose"
+      )
+    )
+  }
+
+val printLogs by
+  tasks.registering(DockerLogsContainer::class) {
+    dependsOn(startContainer)
+    targetContainerId(createContainer.containerId)
+    follow.set(true)
+    tailAll.set(true)
+  }
+
+val startContainer by
+  tasks.creating(DockerStartContainer::class) {
+    dependsOn(createContainer)
+    targetContainerId(createContainer.containerId)
+  }
