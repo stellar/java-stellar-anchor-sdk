4834daadd9ede75305b43211fa246fbc837d735d
Reece Markowsky <reece@stellar.org>
2023-12-13 03:56:28 -0800

    test InvalidConfigException

--- core/src/test/kotlin/org/stellar/anchor/util/Sep1HelperTest.kt
+++ core/src/test/kotlin/org/stellar/anchor/util/Sep1HelperTest.kt
@@ -1,11 +1,8 @@
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
-import java.io.IOException
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.stellar.anchor.api.exception.InvalidConfigException
 import org.stellar.anchor.util.NetUtil
@@ -16,58 +13,6 @@ internal class Sep1HelperTest {
   @AfterEach
   fun tearDown() {
     unmockkAll()
-  }
-
-  @Test
-  fun `readToml returns TomlContent on successful fetch`() {
-    val mockUrl = "http://example.com/stellar.toml"
-    val validTomlContent =
-      """
-            FEDERATION_SERVER = "https://api.example.com/federation"
-            AUTH_SERVER = "https://api.example.com/auth"
-            TRANSFER_SERVER = "https://api.example.com/transfer"
-            SIGNING_KEY = "GABCDEFGHJKLMNOPQRSTUVWXYZ1234567"
-            """
-        .trimIndent()
-    mockkStatic(NetUtil::class)
-    every { NetUtil.fetch(mockUrl) } returns validTomlContent
-
-    val result = Sep1Helper.readToml(mockUrl)
-    assertEquals("https://api.example.com/federation", result.getString("FEDERATION_SERVER"))
-    assertEquals("https://api.example.com/auth", result.getString("AUTH_SERVER"))
-    assertEquals("https://api.example.com/transfer", result.getString("TRANSFER_SERVER"))
-    assertEquals("GABCDEFGHJKLMNOPQRSTUVWXYZ1234567", result.getString("SIGNING_KEY"))
-  }
-
-  @Test
-  fun `readToml throws IOException on network error with trycatch`() {
-    val mockUrl = "http://example.com/stellar.toml"
-    mockkStatic(NetUtil::class)
-    every { NetUtil.fetch(mockUrl) } throws IOException("Network error")
-
-    try {
-      Sep1Helper.readToml(mockUrl)
-      fail("IOException expected")
-    } catch (e: IOException) {
-      // Expected exception
-    }
-  }
-
-  @Test
-  fun `readToml throws InvalidConfigException on invalid TOML content`() {
-    val mockUrl = "http://example.com/stellar.toml"
-    val invalidTomlContent = "Invalid TOML Content"
-    mockkStatic(NetUtil::class)
-    every { NetUtil.fetch(mockUrl) } returns invalidTomlContent
-
-    var exceptionThrown = false
-    try {
-      Sep1Helper.readToml(mockUrl)
-    } catch (e: InvalidConfigException) {
-      print("Caught InvalidConfigException")
-      exceptionThrown = true
-    }
-    assertTrue(exceptionThrown, "InvalidConfigException was expected but not thrown")
   }
 
   @Test
