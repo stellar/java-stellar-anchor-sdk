ec44403ef66cc8a786d0606b4c4af965bbae22d8
Reece Markowsky <reece@stellar.org>
2023-12-13 09:08:53 -0800

    LockAndMock

--- core/src/test/kotlin/org/stellar/anchor/util/SepHelperTest.kt
+++ core/src/test/kotlin/org/stellar/anchor/util/SepHelperTest.kt
@@ -1,11 +1,16 @@
 package org.stellar.anchor.util
 
+import io.mockk.every
+import io.mockk.mockkStatic
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
+import org.stellar.anchor.LockAndMockStatic
+import org.stellar.anchor.api.exception.InvalidConfigException
 import org.stellar.sdk.xdr.MemoType
 
 internal class SepHelperTest {
@@ -45,5 +50,29 @@ internal class SepHelperTest {
   )
   fun `test invalid stellar account`(strAccount: String) {
     assertThrows<Exception> { SepHelper.getAccountMemo(strAccount) }
+  }
+
+  @Test
+  @LockAndMockStatic([NetUtil::class])
+  fun `readToml throws InvalidConfigException when fetching instance metadata instead of TOML`() {
+    val mockUrl = "http://169.254.169.254/latest/meta-data/local-hostname"
+    val instanceMetadataContent =
+      """
+        hostname: ec2-instance-hostname
+        ami-id: ami-1234567890abcdef0
+        instance-id: i-1234567890abcdef0
+        instance-type: t2.micro
+        """
+        .trimIndent()
+    mockkStatic(NetUtil::class)
+    every { NetUtil.fetch(mockUrl) } returns instanceMetadataContent
+
+    var exceptionThrown = false
+    try {
+      Sep1Helper.readToml(mockUrl)
+    } catch (e: InvalidConfigException) {
+      exceptionThrown = true
+    }
+    assertTrue(exceptionThrown, "InvalidConfigException was expected but not thrown")
   }
 }
