b3295358d0346cb4b4349d80efc57e97cd790c9e
jiahuihu <jiahuihu0425@gmail.com>
2024-01-09 16:03:55 -0500

    Change message db column type from VARCHAR to TEXT

---
+++ platform/src/main/resources/db/migration/V13__update_message_column_to_text.sql
@@ -0,0 +1,9 @@
+ALTER TABLE sep6_transaction ALTER COLUMN message TYPE TEXT;
+
+ALTER TABLE sep6_transaction ALTER COLUMN required_info_message TYPE TEXT;
+
+ALTER TABLE sep6_transaction ALTER COLUMN required_customer_info_message TYPE TEXT;
+
+ALTER TABLE sep24_transaction ALTER COLUMN message TYPE TEXT;
+
+ALTER TABLE sep31_transaction ALTER COLUMN required_info_message TYPE TEXT;
