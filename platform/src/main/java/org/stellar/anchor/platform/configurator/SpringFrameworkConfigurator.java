c3b82a630c175b18bd920a570b50ca20e225ae33
Gleb <gleb@stellar.org>
2024-02-08 13:28:02 -0800

    Fix log level bug

--- platform/src/main/java/org/stellar/anchor/platform/configurator/SpringFrameworkConfigurator.java
+++ platform/src/main/java/org/stellar/anchor/platform/configurator/SpringFrameworkConfigurator.java
@@ -1,6 +1,8 @@
 package org.stellar.anchor.platform.configurator;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.PropertiesPropertySource;
@@ -11,6 +13,15 @@ public class SpringFrameworkConfigurator extends AbstractConfigurator
   public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
     // Load and add the data access settings to Spring `Environment`
     PropertiesPropertySource pps = createPrefixedPropertySource("spring");
+    pps.getSource()
+        .forEach(
+            (String k, Object v) -> {
+              if (k.startsWith("logging.level")) {
+                LoggingSystem system =
+                    LoggingSystem.get(SpringFrameworkConfigurator.class.getClassLoader());
+                system.setLogLevel(k.replace("logging.level.", ""), LogLevel.valueOf((String) v));
+              }
+            });
     applicationContext.getEnvironment().getPropertySources().addFirst(pps);
   }
 }
