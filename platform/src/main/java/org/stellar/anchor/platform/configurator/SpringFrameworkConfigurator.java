e13816a641df208786988ba9e44640e11bfe292e
Gleb <gleb@stellar.org>
2024-02-08 16:08:22 -0800

    [ANCHOR-610] Fix log level (#1268)
    ### Description
    
    Remove hardcoded DEBUG level of Stellar and add support of dynamic
    logging configuration reload
    
    ### Context
    
    Previously, it was impossible to set DEBUG level to Stellar level config
    
    ### Testing
    
    Manually tested with different log levels
    
    ### Documentation
    
    N/A
    
    ### Known limitations
    
    N/A

--- platform/src/main/java/org/stellar/anchor/platform/configurator/SpringFrameworkConfigurator.java
+++ platform/src/main/java/org/stellar/anchor/platform/configurator/SpringFrameworkConfigurator.java
@@ -1,6 +1,8 @@
 package org.stellar.anchor.platform.configurator;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.PropertiesPropertySource;
@@ -11,6 +13,16 @@ public class SpringFrameworkConfigurator extends AbstractConfigurator
   public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
     // Load and add the data access settings to Spring `Environment`
     PropertiesPropertySource pps = createPrefixedPropertySource("spring");
+    pps.getSource()
+        .forEach(
+            (String cfgName, Object cfgValue) -> {
+              if (cfgName.startsWith("logging.level") && cfgValue instanceof String) {
+                LoggingSystem system =
+                    LoggingSystem.get(SpringFrameworkConfigurator.class.getClassLoader());
+                system.setLogLevel(
+                    cfgName.replace("logging.level.", ""), LogLevel.valueOf((String) cfgValue));
+              }
+            });
     applicationContext.getEnvironment().getPropertySources().addFirst(pps);
   }
 }
