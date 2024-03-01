53b540cf99c27c3bf6d9476444afb3d623364b7c
philipliu <philip.liu@stellar.org>
2024-03-01 17:09:28 -0500

    Add back event routes

--- kotlin-reference-server/src/main/kotlin/org/stellar/reference/event/EventRoute.kt
+++ kotlin-reference-server/src/main/kotlin/org/stellar/reference/event/EventRoute.kt
@@ -1,6 +1,7 @@
 package org.stellar.reference.event
 
 import com.google.gson.Gson
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -20,6 +21,28 @@ fun Route.event(eventService: EventService) {
       val receivedEvent = gson.fromJson(receivedEventJson, SendEventRequest::class.java)
       eventService.processEvent(receivedEvent)
       call.respond(gson.toJson(SendEventResponse(HttpStatus.SC_OK, "event processed")))
+    }
+  }
+  route("/events") {
+    // Test endpoint to get the events recorded by the reference server.
+    // The `txnId` parameter is optional. If it is provided, only the events with the given `txnId`
+    // will be returned.
+    get { call.respond(gson.toJson(eventService.getEvents(call.parameters["txnId"]))) }
+    // Test endpoint to clear the events recorded by the reference server.
+    delete {
+      eventService.clearEvents()
+      call.respond("Events cleared")
+    }
+  }
+  route("/events/latest") {
+    // Test endpoint to get the latest event recorded by the reference server.
+    get {
+      val latestEvent = eventService.getLatestEvent()
+      if (latestEvent != null) {
+        call.respond(gson.toJson(latestEvent))
+      } else {
+        call.respond(HttpStatusCode.NotFound)
+      }
     }
   }
 }
