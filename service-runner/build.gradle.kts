6d7ec76bb79f049a9e4b2f340c86529e9025eb37
Jiahui Hu <jiahui.hu@stellar.org>
2024-03-27 12:24:47 -0600

    [ANCHOR-640] Remove SEP10 home_domain (#1307)
    ### Description
    
    The `home_domain` was retained for backward compatibility purposes.  
    Removed as part of the efforts for v3.0.
    
    ### Testing
    
    - `./gradlew test`
    
    ---------
    
    Co-authored-by: Philip Liu <12836897+philipliu@users.noreply.github.com>

--- service-runner/build.gradle.kts
+++ service-runner/build.gradle.kts
@@ -108,7 +108,7 @@ val dockerCreateAnchorTest by
       dependsOn(dockerPullAnchorTest)
       targetImageId { dockerPullAnchorTest.image.get() }
 
-      val homeDomain = System.getenv("TEST_HOME_DO`MAIN") ?: "http://host.docker.internal:8080"
+      val homeDomain = System.getenv("TEST_HOME_DOMAIN") ?: "http://host.docker.internal:8080"
       println("TEST_HOME_DOMAIN=$homeDomain")
       val seps = System.getenv().getOrDefault("TEST_SEPS", "1,6,10,12,24,31,38").split(",")
       println("TEST_SEPS=$seps")
