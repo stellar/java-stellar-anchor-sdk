7d3f6c19dc805a678ca3ccadeb0ee1ef5f96f413
Gleb <gleb@stellar.org>
2024-05-08 14:20:01 -0700

    Add withdrawal support for notify_refund_pending RPC method (#1353)
    ### Description
    
    Add support for withdrawals for existing `notify_refund_pending` RPC
    method
    
    ### Context
    
    I noticed refunds are not available for `pending_user_transfer_complete`
    and `pending_external statuses`.
    
    Generally speaking, in the regular flow transaction goes from
    `pending_user_transfer_start` -> `pending_anchor` -> `completed`
    However, when the anchor takes times to process a transaction and it's
    still in `pending_anchor` , user wants to refund it and can do so.
    In the cases where status is `pending_user_transfer_complete` (funds are
    available for pickup) or `pending_external` (funds are processed by
    external entity) we want to fist move it to `pending_anchor` to indicate
    that funds are not available for pickup anymore, (or that bank transfer
    was cancelled), i.e. indicate that refund has been initiated.
    In this case, if we have issues with Stellar transfer or Circle, etc.
    transaction won't be in status that shows user wrong information.
    (Especially in `pending_user_transfer_complete` case, where user funds
    is technically no longer available for pickup anymore, but status
    doesn't show that)
    After that, we can use existing `notify_refund_send` RPC.
    
    ### Testing
    
    - `./gradlew test`
    - Added new test cases 
    
    ### Documentation
    
    Flow charts should be updated
    (https://stellarorg.atlassian.net/browse/ANCHOR-688)
    
    ### Known limitations
    
    N/A

--- core/src/test/java/org/stellar/anchor/sep24/PojoSep24Refunds.java
+++ core/src/test/java/org/stellar/anchor/sep24/PojoSep24Refunds.java
@@ -8,4 +8,9 @@ public class PojoSep24Refunds implements Sep24Refunds {
   String amountRefunded;
   String amountFee;
   List<Sep24RefundPayment> refundPayments;
+
+  @Override
+  public boolean hasRefundPayments() {
+    return refundPayments != null;
+  }
 }
