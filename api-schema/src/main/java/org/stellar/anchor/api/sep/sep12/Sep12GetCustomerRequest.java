ee09da6bc89033ef62e6508ce944a5bc1ee420e8
Jamie Li <lijamie@gmail.com>
2024-09-25 08:48:33 -0700

    [ANCHOR-834] Add transaction_id to callback customer API when request provides it (#1519)
    ### Description
    
    -  Add transaction_id to callback customer API when request provides it
    
    ### Context
    
    - When the business server only implements the `GET /custemer` with
    `transaction_id`, it is necessary for the SEP-12 to pass the
    `transaction_id`.
    
    ### Testing
    
    - `./gradlew test`
    Added more tests to `Sep12ServiceTest.kt`
    
    ### Documentation
    
    N/A
    
    ### Known limitations
    
    N/A

--- api-schema/src/main/java/org/stellar/anchor/api/sep/sep12/Sep12GetCustomerRequest.java
+++ api-schema/src/main/java/org/stellar/anchor/api/sep/sep12/Sep12GetCustomerRequest.java
@@ -1,8 +1,7 @@
 package org.stellar.anchor.api.sep.sep12;
 
-import com.google.gson.annotations.SerializedName;
-import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 /**
  * The request body of the GET /customer endpoint of SEP-12.
@@ -12,18 +11,8 @@ import lombok.Data;
  *     to SEP-12</a>
  */
 @Data
-@Builder
-public class Sep12GetCustomerRequest implements Sep12CustomerRequestBase {
-  String id;
-  String account;
-  String memo;
-
-  @SerializedName("transaction_id")
-  String transactionId;
-
-  @SerializedName("memo_type")
-  String memoType;
-
+@SuperBuilder
+public class Sep12GetCustomerRequest extends Sep12CustomerRequestBase {
   String type;
   String lang;
 }
--- core/src/test/kotlin/org/stellar/anchor/sep12/Sep12ServiceTest.kt
+++ core/src/test/kotlin/org/stellar/anchor/sep12/Sep12ServiceTest.kt
@@ -12,6 +12,9 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 import org.skyscreamer.jsonassert.JSONAssert
 import org.stellar.anchor.api.callback.*
 import org.stellar.anchor.api.event.AnchorEvent
@@ -40,6 +43,7 @@ class Sep12ServiceTest {
       "MBFZNZTFSI6TWLVAID7VOLCIFX2PMUOS2X7U6H4TNK4PAPSHPWMMUAAAAAAAAAPCIA2IM"
     private const val CLIENT_DOMAIN = "demo-wallet.stellar.org"
     private const val TEST_HOST_URL = "http://localhost:8080"
+    private const val TEST_TRANSACTION_ID = "test-transaction-id"
     private const val wantedSep12GetCustomerResponse =
       """
 {
@@ -189,15 +193,15 @@ class Sep12ServiceTest {
         )
         .build()
     every { platformApiClient.getTransaction(any()) } returns transaction
-    val jwtToken = createJwtToken(TEST_ACCOUNT)
 
-    val putRequestBase = Sep12PutCustomerRequest.builder().transactionId("id").build()
-    assertDoesNotThrow { sep12Service.validateGetOrPutRequest(putRequestBase, jwtToken) }
-    assertEquals(TEST_ACCOUNT, putRequestBase.account)
-    assertEquals(TEST_MEMO, putRequestBase.memo)
+    val putRequest = Sep12PutCustomerRequest.builder().transactionId(TEST_TRANSACTION_ID).build()
+    assertDoesNotThrow { sep12Service.populateRequestFromTransactionId(putRequest) }
+    assertEquals(TEST_ACCOUNT, putRequest.account)
+    assertEquals(TEST_MEMO, putRequest.memo)
 
-    val getRequestBase = Sep12GetCustomerRequest.builder().transactionId("id").build()
-    assertDoesNotThrow { sep12Service.validateGetOrPutRequest(getRequestBase, jwtToken) }
+    val getRequestBase =
+      Sep12GetCustomerRequest.builder().transactionId(TEST_TRANSACTION_ID).build()
+    assertDoesNotThrow { sep12Service.populateRequestFromTransactionId(getRequestBase) }
     assertEquals(TEST_ACCOUNT, getRequestBase.account)
     assertEquals(TEST_MEMO, getRequestBase.memo)
   }
@@ -255,8 +259,10 @@ class Sep12ServiceTest {
     assertEquals("Invalid 'memo' for 'memo_type'", ex.message)
   }
 
-  @Test
-  fun `Test put customer request ok`() {
+  @ParameterizedTest
+  @ValueSource(strings = [TEST_TRANSACTION_ID])
+  @NullSource
+  fun `Test put customer request ok`(transactionId: String?) {
     // mock `PUT {callbackApi}/customer` response
     val callbackApiPutRequestSlot = slot<PutCustomerRequest>()
     val callbackApiGetRequestSlot = slot<GetCustomerRequest>()
@@ -269,6 +275,14 @@ class Sep12ServiceTest {
     every { customerIntegration.getCustomer(capture(callbackApiGetRequestSlot)) } returns
       mockCallbackApiGetCustomerResponse
     every { eventSession.publish(capture(kycUpdateEventSlot)) } returns Unit
+    every { platformApiClient.getTransaction(any()) } returns
+      GetTransactionResponse.builder()
+        .customers(
+          Customers.builder()
+            .sender(StellarId.builder().account(TEST_ACCOUNT).memo(TEST_MEMO).build())
+            .build()
+        )
+        .build()
 
     // Execute the request
     val mockPutRequest =
@@ -286,6 +300,7 @@ class Sep12ServiceTest {
         .mobileMoneyNumber("12345678")
         .mobileMoneyProvider("M-PESA")
         .externalTransferMemo("memo")
+        .transactionId(transactionId)
         .build()
     val jwtToken = createJwtToken(TEST_ACCOUNT)
     assertDoesNotThrow { sep12Service.putCustomer(jwtToken, mockPutRequest) }
@@ -306,10 +321,12 @@ class Sep12ServiceTest {
         .mobileMoneyNumber("12345678")
         .mobileMoneyProvider("M-PESA")
         .externalTransferMemo("memo")
+        .transactionId(transactionId)
         .build()
     assertEquals(wantCallbackApiPutRequest, callbackApiPutRequestSlot.captured)
 
-    val wantCallbackApiGetCustomerResponse = GetCustomerRequest.builder().id("customer-id").build()
+    val wantCallbackApiGetCustomerResponse =
+      GetCustomerRequest.builder().id("customer-id").transactionId(transactionId).build()
     assertEquals(wantCallbackApiGetCustomerResponse, callbackApiGetRequestSlot.captured)
 
     // validate the published event
